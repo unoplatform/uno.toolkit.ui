266eb8d1e959a4785d06617847e188f69ef0a23e
Luke Blevins <lukeblevins15@outlook.com>
2021-08-26 11:44:37 -0400

    feat: TemplateSettings for TabBar SelectionIndicatorWidth (#53)
    * feat: TemplateSettings for TabBar SelectionIndicatorWidth
    
    Make sample page leverage new property
    
    fix: Assorted improvements based on feedback
    
    * fix: fix for WinUI namespaces
    
    Co-authored-by: Steve Bilogan <steve.bilogan@gmail.com>

---
+++ src/Uno.Toolkit.UI/TabBar/TabBarTemplateSettings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#if IS_WINUI
+using Microsoft.UI.Xaml;
+#else
+using Windows.UI.Xaml;
+#endif
+
+namespace Uno.UI.ToolkitLib
+{
+	public partial class TabBarTemplateSettings : DependencyObject
+	{
+		internal static readonly DependencyProperty SelectionIndicatorWidthProperty =
+			DependencyProperty.Register(nameof(SelectionIndicatorWidth),
+				typeof(double),
+				typeof(TabBarTemplateSettings),
+				new PropertyMetadata(null));
+
+		public double SelectionIndicatorWidth
+		{
+			get => (double)GetValue(SelectionIndicatorWidthProperty);
+			internal set => SetValue(SelectionIndicatorWidthProperty, value);
+		}
+	}
+}
