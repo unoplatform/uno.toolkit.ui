2c9a22c6bae5c555090617e5a6096df7072d0588
xiaoy312 <xiaoyao312@gmail.com>
2021-09-09 12:05:33 -0400

    feat: added broken sample to illustrate nventive-private#271

---
+++ src/Uno.Toolkit.UI/DrawerControl/ManipulationBlocker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+#if IS_WINUI
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+#else
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+#endif
+
+namespace Uno.UI.ToolkitLib
+{
+	public partial class ManipulationBlocker : ContentControl
+	{
+		protected override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+			this.ManipulationMode = ManipulationModes.TranslateX | ManipulationModes.TranslateY;
+			this.ManipulationStarted += (s, e) => e.Handled = true;
+		}
+	}
+}
