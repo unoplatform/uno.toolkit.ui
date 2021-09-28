c976b12cd268ee8ce81c052662906ca562fe56a9
Agnès Zitte <agnes.zitte@nventive.com>
2021-09-28 10:44:17 -0400

    feat(TabBar): Add Material Cupertino first draft styling for Top / Bottom TabBar

--- samples/Uno.Toolkit.Samples/Uno.Toolkit.Samples.Shared/Shell.xaml.cs
+++ samples/Uno.Toolkit.Samples/Uno.Toolkit.Samples.Shared/Shell.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Windows.UI.Core;
+using Uno.Toolkit.Samples.Helpers;
 
 #if IS_WINUI
 using Microsoft.UI.Xaml;
@@ -33,6 +35,10 @@ namespace Uno.Toolkit.Samples
 			this.InitializeComponent();
 
 			InitializeSafeArea();
+			this.Loaded += OnLoaded;
+
+			NestedSampleFrame.RegisterPropertyChangedCallback(ContentControl.ContentProperty, OnNestedSampleFrameChanged);
+			SystemNavigationManager.GetForCurrentView().BackRequested += (s, e) => e.Handled = BackNavigateFromNestedSample();
 		}
 
 		public static Shell GetForCurrentView() => (Shell)XamlWindow.Current.Content;
@@ -48,6 +54,29 @@ namespace Uno.Toolkit.Samples
 		public static DependencyProperty CurrentSampleBackdoorProperty { get; } =
 			DependencyProperty.Register(nameof(CurrentSampleBackdoor), typeof(string), typeof(Shell), new PropertyMetadata(null));
 
+		private void OnLoaded(object sender, RoutedEventArgs e)
+		{
+			SetDarkLightToggleInitialState();
+		}
+
+		private void SetDarkLightToggleInitialState()
+		{
+			// Initialize the toggle to the current theme.
+			var root = global::Windows.UI.Xaml.Window.Current.Content as FrameworkElement;
+
+			switch (root.ActualTheme)
+			{
+				case ElementTheme.Default:
+					DarkLightModeToggle.IsChecked = SystemThemeHelper.GetSystemApplicationTheme() == ApplicationTheme.Dark;
+					break;
+				case ElementTheme.Light:
+					DarkLightModeToggle.IsChecked = false;
+					break;
+				case ElementTheme.Dark:
+					DarkLightModeToggle.IsChecked = true;
+					break;
+			}
+		}
 
 		/// <summary>
 		/// This method handles the top padding for phones like iPhone X.
@@ -63,6 +92,92 @@ namespace Uno.Toolkit.Samples
 			{
 				TopPaddingRow.Height = new GridLength(topPadding);
 			}
+		}
+
+		private void ToggleButton_Click(object sender, RoutedEventArgs e)
+		{
+			// Set theme for window root.
+			if (global::Windows.UI.Xaml.Window.Current.Content is FrameworkElement root)
+			{
+				switch (root.ActualTheme)
+				{
+					case ElementTheme.Default:
+						if (SystemThemeHelper.GetSystemApplicationTheme() == ApplicationTheme.Dark)
+						{
+							root.RequestedTheme = ElementTheme.Light;
+						}
+						else
+						{
+							root.RequestedTheme = ElementTheme.Dark;
+						}
+						break;
+					case ElementTheme.Light:
+						root.RequestedTheme = ElementTheme.Dark;
+						break;
+					case ElementTheme.Dark:
+						root.RequestedTheme = ElementTheme.Light;
+						break;
+				}
+
+				if (NavigationViewControl.PaneDisplayMode == MUXC.NavigationViewPaneDisplayMode.LeftMinimal)
+				{
+					// Close navigation view when changing the theme
+					// to allow the user to see the difference between the themes.
+					NavigationViewControl.IsPaneOpen = false;
+				}
+			}
+		}
+
+		private void OnNestedSampleFrameChanged(DependencyObject sender, DependencyProperty dp)
+		{
+			var isInsideNestedSample = NestedSampleFrame.Content != null;
+
+			// prevent empty frame from blocking the content (nav-view) behind it
+			NestedSampleFrame.Visibility = isInsideNestedSample
+				? Visibility.Visible
+				: Visibility.Collapsed;
+
+			// toggle built-in back button for wasm (from browser) and uwp (on title bar)
+			SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = isInsideNestedSample
+				? AppViewBackButtonVisibility.Visible
+				: AppViewBackButtonVisibility.Collapsed;
+		}
+
+		public void ShowNestedSample<TPage>(bool? clearStack = null) where TPage : Page
+		{
+			var wasFrameEmpty = NestedSampleFrame.Content == null;
+			if (NestedSampleFrame.Navigate(typeof(TPage)) && (clearStack ?? wasFrameEmpty))
+			{
+				NestedSampleFrame.BackStack.Clear();
+			}
+		}
+
+		public bool BackNavigateFromNestedSample()
+		{
+			if (NestedSampleFrame.Content == null)
+			{
+				return false;
+			}
+
+			if (NestedSampleFrame.CanGoBack)
+			{
+				NestedSampleFrame.GoBack();
+			}
+			else
+			{
+				NestedSampleFrame.Content = null;
+
+#if __IOS__
+				// This will force reset the UINavigationController, to prevent the back button from appearing when the stack is supposely empty.
+				// note: Merely setting the Frame.Content to null, doesnt fully reset the stack.
+				// When revisiting the page1 again, the previous page1 is still in the UINavigationController stack
+				// causing a back button to appear that takes us back to the previous page1
+				NestedSampleFrame.BackStack.Add(default);
+				NestedSampleFrame.BackStack.Clear();
+#endif
+			}
+
+			return true;
 		}
 
 		private void NavViewToggleButton_Click(object sender, RoutedEventArgs e)
---
+++ src/library/Uno.Toolkit.Material/MaterialResources.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#if IS_WINUI
+using Microsoft.UI.Xaml;
+#else
+using Windows.UI.Xaml;
+#endif
+
+/// <summary>
+/// Material resources
+/// </summary>
+namespace Uno.Toolkit.Material
+{
+	public sealed class MaterialResources : ResourceDictionary
+	{
+		private const string PackageName = "Uno.UI.Toolkit.Material";
+
+		public MaterialResources()
+		{
+			this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri($"ms-appx:///{PackageName}/Styles/Controls/BottomTabBar.xaml") });
+			this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri($"ms-appx:///{PackageName}/Styles/Controls/StandardTabBar.xaml") });
+		}
+	}
+}
