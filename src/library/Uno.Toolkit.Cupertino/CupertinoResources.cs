9a22b46a2e66207b7f962b62cb95ef6a1d55da83
Agnès Zitte <agnes.zitte@nventive.com>
2021-09-29 10:37:38 -0400

    feat(TabBar): Add Material Cupertino first draft styling for Top / Bottom TabBar

---
+++ src/library/Uno.Toolkit.Cupertino/CupertinoResources.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#if IS_WINUI
+using Microsoft.UI.Xaml;
+#else
+using Windows.UI.Xaml;
+#endif
+
+/// <summary>
+/// Cupertino resources
+/// </summary>
+namespace Uno.Toolkit.Cupertino
+{
+	public sealed class CupertinoResources : ResourceDictionary
+	{
+		private const string PackageName = "Uno.UI.Toolkit.Cupertino";
+
+		public CupertinoResources()
+		{
+			this.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri($"ms-appx:///{PackageName}/Styles/Controls/BottomTabBar.xaml") });
+		}
+	}
+}
