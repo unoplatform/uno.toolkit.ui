7b054daaec687d6d8c1095cd0c7c4f09e2735ef4
Nick Randolph <nick@builttoroam.com>
2022-10-14 23:18:17 +1100

    feat: Add WinUI net6 mobile target for sample

--- src/Uno.Toolkit.UI/AssemblyInfo.cs
+++ src/Uno.Toolkit.UI/AssemblyInfo.cs
@@ -7,6 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("Uno.Toolkit.WinUI.Samples")]
 [assembly: InternalsVisibleTo("Uno.Toolkit.WinUI.Samples.Droid")]
 [assembly: InternalsVisibleTo("Uno.Toolkit.WinUI.Samples.iOS")]
+[assembly: InternalsVisibleTo("Uno.Toolkit.WinUI.Samples.Mobile")]
 [assembly: InternalsVisibleTo("Uno.Toolkit.WinUI.Samples.Windows.Desktop")]
 [assembly: InternalsVisibleTo("Uno.Toolkit.WinUI.Samples.macOS")]
 [assembly: InternalsVisibleTo("Uno.Toolkit.WinUI.Samples.WASM")]
