f85b6e2a901d84086d3bf535ebb6c69d59766335
Steve Bilogan <steve.bilogan@gmail.com>
2022-10-17 15:57:35 -0400

    ci: include net6 builds

--- build/workflow/scripts/macos-build.sh
+++ build/workflow/scripts/macos-build.sh
@@ -4,4 +4,4 @@ IFS=$'\n\t'
 
 cd $BUILD_SOURCESDIRECTORY
 
-mono "/Applications/Visual Studio.app/Contents/MonoBundle/MSBuild/Current/bin/MSBuild.dll" /m /r /p:UnoUIUseRoslynSourceGenerators=False /p:Configuration=Release /p:Platform=$BUILD_PLATFORM /p:PackageVersion=$BUILD_PACKAGEVERSION /p:DisableNet6MobileTargets=$BUILD_DISABLEMOBILETARGETS /p:GeneratePackageOnBuild=$BUILD_GENERATEPACKAGE /detailedsummary /bl:$BUILD_BINLOG $BUILD_SOLUTION
+mono "/Applications/Visual Studio.app/Contents/MonoBundle/MSBuild/Current/bin/MSBuild.dll" /m /r /p:UnoUIUseRoslynSourceGenerators=False /p:Configuration=Release /p:Platform=$BUILD_PLATFORM /p:PackageVersion=$BUILD_PACKAGEVERSION /p:GeneratePackageOnBuild=$BUILD_GENERATEPACKAGE /detailedsummary /bl:$BUILD_BINLOG $BUILD_SOLUTION
