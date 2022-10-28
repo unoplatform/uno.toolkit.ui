77a229c59141d09179d7dcb6088c7a7fda87bc6e
Pedro Jesus <pedrojesus.cefet@gmail.com>
2022-10-28 15:01:27 -0300

    fix: changed code to follow the project's code style
    handle null values
    
    handled loaded/unloaded events correctly
    
    fixed null check on SelectorExtensions
    
    changed event subscription
    
    code style
    
    always subscribe loaded/unloaded
    
    changed to use the fullName
    
    code style
    
    usaged of WeakAttachedDictionary collection

--- src/Uno.Toolkit.UI/Behaviors/FlipViewExtensions.cs
+++ src/Uno.Toolkit.UI/Behaviors/FlipViewExtensions.cs
@@ -23,7 +23,9 @@ using Windows.UI.Xaml.Input;
 #endif
 
 namespace Uno.Toolkit.UI;
-
+/// <summary>
+/// Extensions for <see cref="FlipView"/>
+/// </summary>
 public static class FlipViewExtensions
 {
 	#region DependencyProperty: Next
@@ -32,7 +34,7 @@ public static class FlipViewExtensions
 
 	public static void SetNext(Button element, FlipView value) => element.SetValue(NextProperty, value);
 
-	public static FlipView GetNext(Button element) => (FlipView)element.GetValue(NextProperty);
+	public static FlipView? GetNext(Button element) => (FlipView?)element.GetValue(NextProperty);
 
 	#endregion
 
@@ -42,70 +44,122 @@ public static class FlipViewExtensions
 
 	public static void SetPrevious(Button element, FlipView value) => element.SetValue(PreviousProperty, value);
 
-	public static FlipView GetPrevious(Button element) => (FlipView)element.GetValue(PreviousProperty);
+	public static FlipView? GetPrevious(Button element) => (FlipView?)element.GetValue(PreviousProperty);
 	#endregion
 
-	static void OnNextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+	private static void OnNextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
 	{
 		var btn = (ButtonBase)d;
 
 		if (e.NewValue is null)
 		{
-			btn.Click -= OnBtnClick;
+			btn.Click -= OnBtnNextClick;
 			return;
 		}
 
-		var flipView = (FlipView)e.NewValue;
-
-		btn.Click -= OnBtnClick;
-		btn.Click += OnBtnClick;
-
-
-		static void OnBtnClick(object sender, RoutedEventArgs e)
+		if (btn.IsLoaded)
 		{
-			var flipView = GetNext((Button)sender);
-			GoNext(flipView);
+			OnBtnLoaded(btn, default);
+		}
+
+		btn.Loaded += OnBtnLoaded;
+		btn.Unloaded += OnBtnUnloaded;
+
+		static void OnBtnUnloaded(object sender, RoutedEventArgs e)
+		{
+			var btn = (Button)sender;
+
+			btn.Click -= OnBtnNextClick;
+		}
+
+
+		static void OnBtnLoaded(object sender, RoutedEventArgs? e)
+		{
+			var btn = (Button)sender;
+
+			btn.Click += OnBtnNextClick;
 		}
 	}
 
-	static void OnPreviousChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+	private static void OnBtnNextClick(object sender, RoutedEventArgs e)
+	{
+		var flipView = GetNext((Button)sender);
+
+		if (flipView is null)
+		{
+			return;
+		}
+
+		GoNext(flipView);
+	}
+
+	private static void OnPreviousChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
 	{
 		var btn = (ButtonBase)d;
 
 		if (e.NewValue is null)
 		{
-			btn.Click -= OnBtnClick;
+			btn.Click -= OnBtnPreviousClick;
 			return;
 		}
 
-		var flipView = (FlipView)e.NewValue;
-
-		btn.Click -= OnBtnClick;
-		btn.Click += OnBtnClick;
-
-		static void OnBtnClick(object sender, RoutedEventArgs e)
+		if (btn.IsLoaded)
 		{
-			var flipView = GetPrevious((Button)sender);
-			GoBack(flipView);
+			OnBtnLoaded(btn, default);
+		}
+
+		btn.Loaded += OnBtnLoaded;
+		btn.Unloaded += OnBtnUnloaded;
+
+		static void OnBtnUnloaded(object sender, RoutedEventArgs e)
+		{
+			var button = (Button)sender;
+
+			button.Click -= OnBtnPreviousClick;
+		}
+
+
+		static void OnBtnLoaded(object sender, RoutedEventArgs? e)
+		{
+			var button = (Button)sender;
+
+			button.Click += OnBtnPreviousClick;
 		}
 	}
 
-	static void GoBack(FlipView element)
+
+	private static void OnBtnPreviousClick(object sender, RoutedEventArgs e)
+	{
+		var flipView = GetPrevious((Button)sender);
+
+		if (flipView is null)
+		{
+			return;
+		}
+
+		GoBack(flipView);
+	}
+
+	private static void GoBack(FlipView element)
 	{
 		var index = element.SelectedIndex - 1;
 
 		if (index < 0)
+		{
 			return;
+		}
 
 		element.SelectedIndex = index;
 	}
 
-	static void GoNext(FlipView element)
+	private static void GoNext(FlipView element)
 	{
 		var index = element.SelectedIndex + 1;
 
 		if (index >= element.Items.Count)
+		{
 			return;
+		}
 
 		element.SelectedIndex = index;
 	}
--- src/Uno.Toolkit.UI/Behaviors/SelectorExtensions.cs
+++ src/Uno.Toolkit.UI/Behaviors/SelectorExtensions.cs
@@ -10,7 +10,7 @@ using Uno.Logging;
 using Windows.System;
 using Windows.UI.ViewManagement;
 using Windows.Foundation.Collections;
-using Uno.Disposables;
+using Uno.Collections;
 
 #if IS_WINUI
 using Microsoft.UI.Xaml.Data;
@@ -36,7 +36,8 @@ namespace Uno.Toolkit.UI;
 [Bindable]
 public static partial class SelectorExtensions
 {
-	readonly static Dictionary<WeakReference<Selector>, VectorChangedEventHandler<object>> events = new();
+	private readonly static WeakAttachedDictionary<Selector, string> events = new();
+	private const string SelectorAttachedPropertyKey = "Selector";
 
 	/// <summary>
 	/// Backing property for the <see cref="PipsPager"/> that will be linked to the desired <see cref="Selector"/> control.
@@ -48,22 +49,25 @@ public static partial class SelectorExtensions
 	public static void SetPipsPager(Selector element, PipsPager value) =>
 		element.SetValue(PipsPagerProperty, value);
 
-	public static PipsPager GetPipsPager(Selector element) =>
-		(PipsPager)element.GetValue(PipsPagerProperty);
+	public static PipsPager? GetPipsPager(Selector element) =>
+		(PipsPager?)element.GetValue(PipsPagerProperty);
 	#endregion
 
-	static void OnPipsPagerChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+	private static void OnPipsPagerChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
 	{
 		if (args.NewValue == args.OldValue)
+		{
 			return;
+		}
 
 		if (args.OldValue is PipsPager oldPipsPager)
+		{
 			oldPipsPager.SetBinding(PipsPager.SelectedPageIndexProperty, null);
+		}
 
 		var selector = (Selector)dependencyObject;
 		if (args.NewValue is not PipsPager pipsPager)
 		{
-			events.CleanNullKeyReferences();
 			UnsubscribeFromSelectorEvents(selector);
 			return;
 		}
@@ -78,31 +82,47 @@ public static partial class SelectorExtensions
 		pipsPager.SetBinding(PipsPager.SelectedPageIndexProperty, selectedIndexBinding);
 		pipsPager.NumberOfPages = selector.Items.Count;
 
-		UnsubscribeFromSelectorEvents(selector);
-		VectorChangedEventHandler<object> eventHandler = OnItemsVectorChanged;
-		events[new (selector)] = eventHandler;
-		selector.Items.VectorChanged += eventHandler;
-		selector.Unloaded += (_, __) =>
-		{
-			selector.Items.VectorChanged -= eventHandler;
-		};
 
+		if (selector.IsLoaded)
+		{
+			OnSelectorLoaded(selector, default);
+		}
+
+		selector.Loaded += OnSelectorLoaded;
+		selector.Unloaded += OnSelectorUnloaded;
+	}
+
+	private static void OnSelectorUnloaded(object sender, RoutedEventArgs e)
+	{
+		UnsubscribeFromSelectorEvents((Selector)sender);
+	}
+
+	private static void OnSelectorLoaded(object sender, RoutedEventArgs? e)
+	{
+		var selector = (Selector)sender;
+		var pipsPager = GetPipsPager(selector);
+
+		if (pipsPager is null)
+		{
+			return;
+		}
+
+		VectorChangedEventHandler<object> eventHandler = OnItemsVectorChanged;
+		events.SetValue(selector, SelectorAttachedPropertyKey, eventHandler);
+
+		selector.Items.VectorChanged += eventHandler;
 
 		void OnItemsVectorChanged(IObservableVector<object> sender, IVectorChangedEventArgs @event) =>
 			pipsPager.NumberOfPages = selector.Items.Count;
 	}
 
-	static void UnsubscribeFromSelectorEvents(in Selector selector)
+	private static void UnsubscribeFromSelectorEvents(in Selector selector)
 	{
-		var key = events.Keys.GetSelector(selector);
+		var value = events.GetValue<VectorChangedEventHandler<object>>(selector, SelectorAttachedPropertyKey);
 
-		if (key is null)
-			return;
-
-		if (events.TryGetValue(key, out var @event))
+		if (value is not null)
 		{
-			selector.Items.VectorChanged -= @event;
-			events.Remove(key);
+			selector.Items.VectorChanged -= value;
 		}
 	}
 
