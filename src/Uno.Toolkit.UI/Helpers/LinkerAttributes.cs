4b99540948c738ec855b733806fbaa12a5a814b1
Youssef1313 <youssefvictor00@gmail.com>
2022-11-26 00:31:30 +0200

    chore: Fix conditional

--- src/Uno.Toolkit.UI/Helpers/LinkerAttributes.cs
+++ src/Uno.Toolkit.UI/Helpers/LinkerAttributes.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-#if !NET6_0_OR_GREATER || NETSTANDARD2_0
+#if !NET6_0_IOS && !NET6_0_MACCATALYST && !NET6_0_ANDROID
 namespace System.Diagnostics.CodeAnalysis
 {
 	/// <summary>
