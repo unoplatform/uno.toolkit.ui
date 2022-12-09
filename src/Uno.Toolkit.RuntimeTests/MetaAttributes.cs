8dc9c44b5a06c54ed1a528d9035f4909f267b31e
xiaoy312 <xiaoyao312@gmail.com>
2022-12-09 10:51:18 -0500

    feat: runtime tests with uielement

---
+++ src/Uno.Toolkit.RuntimeTests/MetaAttributes.cs
@@ -0,0 +1,11 @@
+﻿namespace System.Runtime.CompilerServices
+{
+#if !NET5 && !NET6_0_OR_GREATER
+	/// <summary>
+	/// Reserved to be used by the compiler for tracking metadata. This class should not be used by developers in source code.
+	/// </summary>
+	internal static class IsExternalInit
+	{
+	}
+#endif
+}
