a761ae2d00713f40c6672cafd9cf6393f82332f8
xiaoy312 <xiaoyao312@gmail.com>
2022-12-12 11:49:17 -0500

    chore: fix build

--- src/library/Uno.Toolkit.Cupertino/Converters/InflateDimensionConverter.cs
+++ src/library/Uno.Toolkit.Cupertino/Converters/InflateDimensionConverter.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml;
 
 namespace Uno.Toolkit.UI
 {
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses", Justification = "Converters")]
 	internal class InflateDimensionConverter : IValueConverter
 	{
 		public double Inflation { get; set; }
