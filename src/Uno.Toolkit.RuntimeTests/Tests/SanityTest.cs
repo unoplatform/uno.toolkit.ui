2930c84a19be9dbfbf32657a8940a15cc699356c
Xiaotian Gu <xiaoyao312@gmail.com>
2022-12-12 13:39:53 -0500

    test: added support for uielement (#409)

--- src/Uno.Toolkit.RuntimeTests/Tests/SanityTest.cs
+++ src/Uno.Toolkit.RuntimeTests/Tests/SanityTest.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Uno.UI.RuntimeTests;
+
+#if IS_WINUI
+using Microsoft.UI.Xaml.Controls;
+#else
+using Windows.UI.Xaml.Controls;
+#endif
 
 namespace Uno.Toolkit.RuntimeTests.Tests;
 
@@ -18,6 +25,17 @@ public class SanityTests
 	public async Task Is_Still_Sane()
 	{
 		await Task.Delay(500);
+	}
+
+	[TestMethod]
+	[RunsOnUIThread]
+	public async Task When_Test_ContentHelper()
+	{
+		var SUT = new TextBlock() { Text = "New paths lead to new nightmares." };
+		UnitTestsUIContentHelper.Content = SUT;
+
+		await UnitTestsUIContentHelper.WaitForIdle();
+		await UnitTestsUIContentHelper.WaitForLoaded(SUT);
 	}
 
 #if DEBUG && false // used for testing the engine
