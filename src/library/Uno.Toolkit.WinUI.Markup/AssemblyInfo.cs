bccaccf0da83e12cfc4d82c2d0866b662d4be47e
Steve Bilogan <steve.bilogan@gmail.com>
2022-12-14 10:51:24 -0500

    chore: prepare for release

--- src/library/Uno.Toolkit.WinUI.Markup/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using Microsoft.UI.Xaml.Generator;
-using System.Reflection;
-
-[assembly: GenerateMarkupForAssembly(typeof(Uno.Toolkit.UI.ToolkitResources))]
-[assembly: AssemblyMetadata("IsTrimmable", "True")]
