2fd48162355418eb0273084460e9287664406c10
AndresAldana <andres.aldana@horus.com.uy>
2022-12-26 12:21:34 -0500

    chore: use ResourceValue for Theme.Styles

--- src/library/Uno.Toolkit.WinUI.Themes.Markup/Theme.Styles.cs
+++ src/library/Uno.Toolkit.WinUI.Themes.Markup/Theme.Styles.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.MarkupHelpers;
+﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.MarkupHelpers.Internals;
+using Uno.Themes.Markup;
 
 namespace Uno.Toolkit.Themes.Markup
 {
@@ -12,67 +11,67 @@ namespace Uno.Toolkit.Themes.Markup
 			public static class AppBarButton
 			{
 				[ResourceKeyDefinition(typeof(Style), "MainCommandStyle", TargetType = typeof(AppBarButton))]
-				public static Action<IDependencyPropertyBuilder<Style>> Default => StaticResource.Get<Style>("MainCommandStyle");
+				public static ResourceValue<Style> Default => new("MainCommandStyle");
 
 				[ResourceKeyDefinition(typeof(Style), "MainCommandStyle", TargetType = typeof(AppBarButton))]
-				public static Action<IDependencyPropertyBuilder<Style>> Modal => StaticResource.Get<Style>("ModalMainCommandStyle");
+				public static ResourceValue<Style> Modal => new("ModalMainCommandStyle");
 
 				[ResourceKeyDefinition(typeof(Style), "MainCommandStyle", TargetType = typeof(AppBarButton))]
-				public static Action<IDependencyPropertyBuilder<Style>> Primary => StaticResource.Get<Style>("PrimaryMainCommandStyle");
+				public static ResourceValue<Style> Primary => new("PrimaryMainCommandStyle");
 
 				[ResourceKeyDefinition(typeof(Style), "MainCommandStyle", TargetType = typeof(AppBarButton))]
-				public static Action<IDependencyPropertyBuilder<Style>> PrimaryModal => StaticResource.Get<Style>("PrimaryModalMainCommandStyle");
+				public static ResourceValue<Style> PrimaryModal => new("PrimaryModalMainCommandStyle");
 
 				[ResourceKeyDefinition(typeof(Style), "MainCommandStyle", TargetType = typeof(AppBarButton))]
-				public static Action<IDependencyPropertyBuilder<Style>> PrimaryAppBar => StaticResource.Get<Style>("PrimaryAppBarButtonStyle");
+				public static ResourceValue<Style> PrimaryAppBar => new("PrimaryAppBarButtonStyle");
 			}
 
 			public static class Card
 			{
 				[ResourceKeyDefinition(typeof(Style), "FilledCardStyle", TargetType = typeof(Card))]
-				public static Action<IDependencyPropertyBuilder<Style>> Filled => StaticResource.Get<Style>("FilledCardStyle");
+				public static ResourceValue<Style> Filled => new("FilledCardStyle");
 
 				[ResourceKeyDefinition(typeof(Style), "OutlinedCardStyle", TargetType = typeof(Card))]
-				public static Action<IDependencyPropertyBuilder<Style>> Outlined => StaticResource.Get<Style>("OutlinedCardStyle");
+				public static ResourceValue<Style> Outlined => new("OutlinedCardStyle");
 
 				[ResourceKeyDefinition(typeof(Style), "ElevatedCardStyle", TargetType = typeof(Card))]
-				public static Action<IDependencyPropertyBuilder<Style>> Elevated => StaticResource.Get<Style>("ElevatedCardStyle");
+				public static ResourceValue<Style> Elevated => new("ElevatedCardStyle");
 
 				public static class Avatar
 				{
 					[ResourceKeyDefinition(typeof(Style), "AvatarFilledCardStyle", TargetType = typeof(Card))]
-					public static Action<IDependencyPropertyBuilder<Style>> Filled => StaticResource.Get<Style>("AvatarFilledCardStyle");
+					public static ResourceValue<Style> Filled => new("AvatarFilledCardStyle");
 
 					[ResourceKeyDefinition(typeof(Style), "AvatarOutlinedCardStyle", TargetType = typeof(Card))]
-					public static Action<IDependencyPropertyBuilder<Style>> Outlined => StaticResource.Get<Style>("AvatarOutlinedCardStyle");
+					public static ResourceValue<Style> Outlined => new("AvatarOutlinedCardStyle");
 
 					[ResourceKeyDefinition(typeof(Style), "AvatarElevatedCardStyle", TargetType = typeof(Card))]
-					public static Action<IDependencyPropertyBuilder<Style>> Elevated => StaticResource.Get<Style>("AvatarElevatedCardStyle");
+					public static ResourceValue<Style> Elevated => new("AvatarElevatedCardStyle");
 				}
 
 				public static class SmallMedia
 				{
 					[ResourceKeyDefinition(typeof(Style), "SmallMediaFilledCardStyle", TargetType = typeof(Card))]
-					public static Action<IDependencyPropertyBuilder<Style>> Filled => StaticResource.Get<Style>("SmallMediaFilledCardStyle");
+					public static ResourceValue<Style> Filled => new("SmallMediaFilledCardStyle");
 
 					[ResourceKeyDefinition(typeof(Style), "SmallMediaOutlinedCardStyle", TargetType = typeof(Card))]
-					public static Action<IDependencyPropertyBuilder<Style>> Outlined => StaticResource.Get<Style>("SmallMediaOutlinedCardStyle");
+					public static ResourceValue<Style> Outlined => new("SmallMediaOutlinedCardStyle");
 
 					[ResourceKeyDefinition(typeof(Style), "SmallMediaElevatedCardStyle", TargetType = typeof(Card))]
-					public static Action<IDependencyPropertyBuilder<Style>> Default => StaticResource.Get<Style>("SmallMediaElevatedCardStyle");
+					public static ResourceValue<Style> Elevated => new("SmallMediaElevatedCardStyle");
 				}
 			}
 
 			public static class CardContentControl
 			{
 				[ResourceKeyDefinition(typeof(Style), "FilledCardContentControlStyle", TargetType = typeof(CardContentControl))]
-				public static Action<IDependencyPropertyBuilder<Style>> Filled => StaticResource.Get<Style>("FilledCardContentControlStyle");
+				public static ResourceValue<Style> Filled => new("FilledCardContentControlStyle");
 
 				[ResourceKeyDefinition(typeof(Style), "OutlinedCardContentControlStyle", TargetType = typeof(CardContentControl))]
-				public static Action<IDependencyPropertyBuilder<Style>> Outlined => StaticResource.Get<Style>("OutlinedCardContentControlStyle");
+				public static ResourceValue<Style> Outlined => new("OutlinedCardContentControlStyle");
 
 				[ResourceKeyDefinition(typeof(Style), "ElevatedCardContentControlStyle", TargetType = typeof(CardContentControl))]
-				public static Action<IDependencyPropertyBuilder<Style>> Elevated => StaticResource.Get<Style>("ElevatedCardContentControlStyle");
+				public static ResourceValue<Style> Elevated => new("ElevatedCardContentControlStyle");
 			}
 
 			public static class Chip
@@ -80,34 +79,34 @@ namespace Uno.Toolkit.Themes.Markup
 				public static class Assist
 				{
 					[ResourceKeyDefinition(typeof(Style), "AssistChipStyle", TargetType = typeof(Chip))]
-					public static Action<IDependencyPropertyBuilder<Style>> Default => StaticResource.Get<Style>("AssistChipStyle");
+					public static ResourceValue<Style> Default => new("AssistChipStyle");
 
 					[ResourceKeyDefinition(typeof(Style), "ElevatedAssistChipStyle", TargetType = typeof(Chip))]
-					public static Action<IDependencyPropertyBuilder<Style>> Elevated => StaticResource.Get<Style>("ElevatedAssistChipStyle");
+					public static ResourceValue<Style> Elevated => new("ElevatedAssistChipStyle");
 				}
 
 				public static class Input
 				{
 					[ResourceKeyDefinition(typeof(Style), "InputChipStyle", TargetType = typeof(Chip))]
-					public static Action<IDependencyPropertyBuilder<Style>> Default => StaticResource.Get<Style>("InputChipStyle");
+					public static ResourceValue<Style> Default => new("InputChipStyle");
 				}
 
 				public static class Filter
 				{
 					[ResourceKeyDefinition(typeof(Style), "FilterChipStyle", TargetType = typeof(Chip))]
-					public static Action<IDependencyPropertyBuilder<Style>> Default => StaticResource.Get<Style>("FilterChipStyle");
+					public static ResourceValue<Style> Default => new("FilterChipStyle");
 
 					[ResourceKeyDefinition(typeof(Style), "ElevatedFilterChipStyle", TargetType = typeof(Chip))]
-					public static Action<IDependencyPropertyBuilder<Style>> Elevated => StaticResource.Get<Style>("ElevatedFilterChipStyle");
+					public static ResourceValue<Style> Elevated => new("ElevatedFilterChipStyle");
 				}
 
 				public static class Suggestion
 				{
 					[ResourceKeyDefinition(typeof(Style), "SuggestionChipStyle", TargetType = typeof(Chip))]
-					public static Action<IDependencyPropertyBuilder<Style>> Default => StaticResource.Get<Style>("SuggestionChipStyle");
+					public static ResourceValue<Style> Default => new("SuggestionChipStyle");
 
 					[ResourceKeyDefinition(typeof(Style), "ElevatedSuggestionChipStyle", TargetType = typeof(Chip))]
-					public static Action<IDependencyPropertyBuilder<Style>> Elevated => StaticResource.Get<Style>("ElevatedSuggestionChipStyle");
+					public static ResourceValue<Style> Elevated => new("ElevatedSuggestionChipStyle");
 				}
 			}
 
@@ -116,92 +115,92 @@ namespace Uno.Toolkit.Themes.Markup
 				public static class Assist
 				{
 					[ResourceKeyDefinition(typeof(Style), "AssistChipGroupStyle", TargetType = typeof(ChipGroup))]
-					public static Action<IDependencyPropertyBuilder<Style>> Default => StaticResource.Get<Style>("AssistChipGroupStyle");
+					public static ResourceValue<Style> Default => new("AssistChipGroupStyle");
 
 					[ResourceKeyDefinition(typeof(Style), "ElevatedAssistChipGroupStyle", TargetType = typeof(ChipGroup))]
-					public static Action<IDependencyPropertyBuilder<Style>> Elevated => StaticResource.Get<Style>("ElevatedAssistChipGroupStyle");
+					public static ResourceValue<Style> Elevated => new("ElevatedAssistChipGroupStyle");
 				}
 
 				public static class Input
 				{
 					[ResourceKeyDefinition(typeof(Style), "InputChipGroupStyle", TargetType = typeof(ChipGroup))]
-					public static Action<IDependencyPropertyBuilder<Style>> Default => StaticResource.Get<Style>("InputChipGroupStyle");
+					public static ResourceValue<Style> Default => new("InputChipGroupStyle");
 				}
 
 				public static class Filter
 				{
 					[ResourceKeyDefinition(typeof(Style), "FilterChipGroupStyle", TargetType = typeof(ChipGroup))]
-					public static Action<IDependencyPropertyBuilder<Style>> Default => StaticResource.Get<Style>("FilterChipGroupStyle");
+					public static ResourceValue<Style> Default => new("FilterChipGroupStyle");
 
 					[ResourceKeyDefinition(typeof(Style), "ElevatedFilterChipGroupStyle", TargetType = typeof(ChipGroup))]
-					public static Action<IDependencyPropertyBuilder<Style>> Elevated => StaticResource.Get<Style>("ElevatedFilterChipGroupStyle");
+					public static ResourceValue<Style> Elevated => new("ElevatedFilterChipGroupStyle");
 				}
 
 				public static class Suggestion
 				{
 					[ResourceKeyDefinition(typeof(Style), "SuggestionChipGroupStyle", TargetType = typeof(ChipGroup))]
-					public static Action<IDependencyPropertyBuilder<Style>> Default => StaticResource.Get<Style>("SuggestionChipGroupStyle");
+					public static ResourceValue<Style> Default => new("SuggestionChipGroupStyle");
 
 					[ResourceKeyDefinition(typeof(Style), "ElevatedSuggestionChipGroupStyle", TargetType = typeof(ChipGroup))]
-					public static Action<IDependencyPropertyBuilder<Style>> Elevated => StaticResource.Get<Style>("ElevatedSuggestionChipGroupStyle");
+					public static ResourceValue<Style> Elevated => new("ElevatedSuggestionChipGroupStyle");
 				}
 			}
 
 			public static class Divider
 			{
 				[ResourceKeyDefinition(typeof(Style), "DividerStyle", TargetType = typeof(Divider))]
-				public static Action<IDependencyPropertyBuilder<Style>> Default => StaticResource.Get<Style>("DividerStyle");
+				public static ResourceValue<Style> Default => new("DividerStyle");
 			}
 
 			public static class FlyoutPresenter
 			{
 				[ResourceKeyDefinition(typeof(Style), "LeftDrawerFlyoutPresenterStyle", TargetType = typeof(FlyoutPresenter))]
-				public static Action<IDependencyPropertyBuilder<Style>> LeftDrawer => StaticResource.Get<Style>("LeftDrawerFlyoutPresenterStyle");
+				public static ResourceValue<Style> LeftDrawer => new("LeftDrawerFlyoutPresenterStyle");
 
 				[ResourceKeyDefinition(typeof(Style), "TopDrawerFlyoutPresenterStyle", TargetType = typeof(FlyoutPresenter))]
-				public static Action<IDependencyPropertyBuilder<Style>> TopDrawer => StaticResource.Get<Style>("TopDrawerFlyoutPresenterStyle");
+				public static ResourceValue<Style> TopDrawer => new("TopDrawerFlyoutPresenterStyle");
 
 				[ResourceKeyDefinition(typeof(Style), "RightDrawerFlyoutPresenterStyle", TargetType = typeof(FlyoutPresenter))]
-				public static Action<IDependencyPropertyBuilder<Style>> RightDrawer => StaticResource.Get<Style>("RightDrawerFlyoutPresenterStyle");
+				public static ResourceValue<Style> RightDrawer => new("RightDrawerFlyoutPresenterStyle");
 
 				[ResourceKeyDefinition(typeof(Style), "BottomDrawerFlyoutPresenterStyle", TargetType = typeof(FlyoutPresenter))]
-				public static Action<IDependencyPropertyBuilder<Style>> BottomDrawer => StaticResource.Get<Style>("BottomDrawerFlyoutPresenterStyle");
+				public static ResourceValue<Style> BottomDrawer => new("BottomDrawerFlyoutPresenterStyle");
 			}
 
 			public static class NavigationBar
 			{
 				[ResourceKeyDefinition(typeof(Style), "NavigationBarStyle", TargetType = typeof(NavigationBar))]
-				public static Action<IDependencyPropertyBuilder<Style>> Default => StaticResource.Get<Style>("NavigationBarStyle");
+				public static ResourceValue<Style> Default => new("NavigationBarStyle");
 
 				[ResourceKeyDefinition(typeof(Style), "ModalNavigationBarStyle", TargetType = typeof(NavigationBar))]
-				public static Action<IDependencyPropertyBuilder<Style>> Modal => StaticResource.Get<Style>("ModalNavigationBarStyle");
+				public static ResourceValue<Style> Modal => new("ModalNavigationBarStyle");
 
 				[ResourceKeyDefinition(typeof(Style), "PrimaryNavigationBarStyle", TargetType = typeof(NavigationBar))]
-				public static Action<IDependencyPropertyBuilder<Style>> Primary => StaticResource.Get<Style>("PrimaryNavigationBarStyle");
+				public static ResourceValue<Style> Primary => new("PrimaryNavigationBarStyle");
 
 				[ResourceKeyDefinition(typeof(Style), "PrimaryModalNavigationBarStyle", TargetType = typeof(NavigationBar))]
-				public static Action<IDependencyPropertyBuilder<Style>> PrimaryModal => StaticResource.Get<Style>("PrimaryModalNavigationBarStyle");
+				public static ResourceValue<Style> PrimaryModal => new("PrimaryModalNavigationBarStyle");
 			}
 
 			public static class TabBar
 			{
 				[ResourceKeyDefinition(typeof(Style), "BottomTabBarStyle", TargetType = typeof(TabBar))]
-				public static Action<IDependencyPropertyBuilder<Style>> Bottom => StaticResource.Get<Style>("BottomTabBarStyle");
+				public static ResourceValue<Style> Bottom => new("BottomTabBarStyle");
 
 				[ResourceKeyDefinition(typeof(Style), "TopTabBarStyle", TargetType = typeof(TabBar))]
-				public static Action<IDependencyPropertyBuilder<Style>> Top => StaticResource.Get<Style>("TopTabBarStyle");
+				public static ResourceValue<Style> Top => new("TopTabBarStyle");
 
 				[ResourceKeyDefinition(typeof(Style), "ColoredTopTabBarStyle", TargetType = typeof(TabBar))]
-				public static Action<IDependencyPropertyBuilder<Style>> Colored => StaticResource.Get<Style>("ColoredTopTabBarStyle");
+				public static ResourceValue<Style> Colored => new("ColoredTopTabBarStyle");
 			}
 
 			public static class TabBarItem
 			{
 				[ResourceKeyDefinition(typeof(Style), "BottomFabTabBarItemStyle", TargetType = typeof(TabBarItem))]
-				public static Action<IDependencyPropertyBuilder<Style>> BottomFab => StaticResource.Get<Style>("BottomFabTabBarItemStyle");
+				public static ResourceValue<Style> BottomFab => new("BottomFabTabBarItemStyle");
 
 				[ResourceKeyDefinition(typeof(Style), "BottomTabBarItemStyle", TargetType = typeof(TabBarItem))]
-				public static Action<IDependencyPropertyBuilder<Style>> Bottom => StaticResource.Get<Style>("BottomTabBarItemStyle");
+				public static ResourceValue<Style> Bottom => new("BottomTabBarItemStyle");
 			}
 		}
 	}
