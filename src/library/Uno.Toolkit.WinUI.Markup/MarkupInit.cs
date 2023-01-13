95e9e69f71328f3b1f73552023e6fc38b6f64513
Steve Bilogan <steve.bilogan@gmail.com>
2023-01-13 11:35:27 -0500

    feat: Add markup init helper for base Toolkit library

---
+++ src/library/Uno.Toolkit.WinUI.Markup/MarkupInit.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uno.Toolkit.UI
+{
+	public static class MarkupInit
+	{
+		public static T UseToolkit<T>(this T app) where T : Application
+			=> app.Resources(r => r.Merged(new ToolkitResources()));
+	}
+}
