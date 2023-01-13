91f077595e06284026b25aa358a945e96f145119
Steve Bilogan <steve.bilogan@gmail.com>
2023-01-13 11:52:30 -0500

    chore: add material markup

---
+++ src/library/Uno.Toolkit.WinUI.Material.Markup/MarkupInit.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Uno.Toolkit.UI.Material;
+
+namespace Uno.Toolkit.UI.Material
+{
+	public static class MarkupInit
+	{
+		public static T UseMaterialToolkit<T>(this T app) where T : Application
+			=> app
+			.UseToolkit()
+			.Resources(r => r.Merged(new MaterialToolkitResources()));
+	}
+}
