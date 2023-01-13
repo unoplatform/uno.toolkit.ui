6beb7ed2e1652755eb386a356728759b22a9216c
Steve Bilogan <steve.bilogan@gmail.com>
2023-01-13 12:00:51 -0500

    chore: comments

--- src/library/Uno.Toolkit.WinUI.Markup/MarkupInit.cs
+++ src/library/Uno.Toolkit.WinUI.Markup/MarkupInit.cs
@@ -5,8 +5,14 @@ using System.Text;
 
 namespace Uno.Toolkit.UI
 {
+	/// <summary>
+	/// Application helper extensions for easy initialization of <see cref="ToolkitResources"/>
+	/// </summary>
 	public static class MarkupInit
 	{
+		/// <summary>
+		/// Initializes and adds the <see cref="ToolkitResources"/> to the MergedDictionaries of <see cref="Application.Resources"/> 
+		/// </summary>
 		public static T UseToolkit<T>(this T app) where T : Application
 			=> app.Resources(r => r.Merged(new ToolkitResources()));
 	}
--- src/library/Uno.Toolkit.WinUI.Material.Markup/MarkupInit.cs
+++ src/library/Uno.Toolkit.WinUI.Material.Markup/MarkupInit.cs
@@ -6,8 +6,14 @@ using Uno.Toolkit.UI.Material;
 
 namespace Uno.Toolkit.UI.Material
 {
+	/// <summary>
+	/// Application helper extensions for easy initialization of <see cref="MaterialToolkitResources"/>
+	/// </summary>
 	public static class MarkupInit
 	{
+		/// <summary>
+		/// Initializes and adds the <see cref="MaterialToolkitResources"/> to the MergedDictionaries of <see cref="Application.Resources"/> 
+		/// </summary>
 		public static T UseMaterialToolkit<T>(this T app) where T : Application
 			=> app
 			.UseToolkit()
