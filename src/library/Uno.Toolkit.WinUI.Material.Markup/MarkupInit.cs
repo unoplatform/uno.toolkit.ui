a73bb874d95eb403abe4f5f9c85bdaef9ee04193
Steve Bilogan <steve.bilogan@gmail.com>
2023-01-15 23:17:37 -0500

    feat: Add markup init helper for base Toolkit library (#447)
    * feat: Add markup init helper for base Toolkit library

---
+++ src/library/Uno.Toolkit.WinUI.Material.Markup/MarkupInit.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Uno.Toolkit.UI.Material;
+
+namespace Uno.Toolkit.UI.Material
+{
+	/// <summary>
+	/// Application helper extensions for easy initialization of <see cref="MaterialToolkitResources"/>
+	/// </summary>
+	public static class MarkupInit
+	{
+		/// <summary>
+		/// Initializes and adds the <see cref="MaterialToolkitResources"/> to the MergedDictionaries of <see cref="Application.Resources"/> 
+		/// </summary>
+		public static T UseMaterialToolkit<T>(this T app) where T : Application
+			=> app
+			.UseToolkit()
+			.Resources(r => r.Merged(new MaterialToolkitResources()));
+	}
+}
