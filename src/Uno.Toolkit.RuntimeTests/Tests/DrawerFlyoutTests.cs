06dd99f73f97be8fc263b3360d66a9a3f783bf60
Agnès Zitte <agnes.zitte@nventive.com>
2023-01-24 16:32:02 -0500

    StartCreatingTestsDrawerFlyout

---
+++ src/Uno.Toolkit.RuntimeTests/Tests/DrawerFlyoutTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Uno.Extensions;
+using Uno.Toolkit.RuntimeTests.Helpers;
+using Uno.Toolkit.UI;
+using Uno.UI.RuntimeTests;
+
+#if IS_WINUI
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Media;
+#else
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Media;
+#endif
+
+namespace Uno.Toolkit.RuntimeTests.Tests;
+
+[TestClass]
+[RunsOnUIThread]
+public class DrawerFlyoutTests
+{
+	/*	Test Plan
+ *		- 
+ */
+
+	[TestMethod]
+	[RunsOnUIThread]
+	public async Task When_Left_DrawerFlyout()
+	{
+		var stackPanel = new StackPanel { MinWidth = 220, VerticalAlignment = VerticalAlignment.Stretch };
+		var printTextBlock = new TextBlock { Text = "Text inside DrawerFlyout" };
+		var btn = new Button { Content = "Button inside DrawerFlyout" };
+		stackPanel.Children.Add(printTextBlock);
+		stackPanel.Children.Add(btn);
+
+		var flyout = new Flyout
+		{
+			Content = stackPanel,
+			Placement = FlyoutPlacementMode.Full,
+			FlyoutPresenterStyle = new Style(typeof(DrawerFlyoutPresenter))
+			{
+				Setters =
+					{
+						new Setter(DrawerFlyoutPresenter.OpenDirectionProperty, DrawerOpenDirection.Left),
+					}
+			}
+		};
+
+		var button = new Button()
+		{
+			Flyout = flyout,
+		};
+
+		await UnitTestUIContentHelperEx.SetContentAndWait(stackPanel);
+
+		Assert.AreEqual(220, stackPanel.ActualWidth);
+
+		//button.Click += (s, e) =>
+		//{
+		//	printTextBlock.Text = "Text inside DrawerFlyout after click";
+		//	btn.Content = "Button inside DrawerFlyout after click";
+		//};
+
+		//button.Click(new object(), new RoutedEventArgs());
+
+		//Assert.AreEqual("Text inside DrawerFlyout after click", printTextBlock.Text);
+		//Assert.AreEqual("Button inside DrawerFlyout after click", btn.Content);
+	}
+}
---
+++ src/Uno.Toolkit.UITest/Controls/DrawerFlyout/Given_DrawerFlyout.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+using Uno.Toolkit.UITest.Extensions;
+using Uno.UITest.Helpers;
+using static System.Net.Mime.MediaTypeNames;
+using Uno.UITest.Helpers.Queries;
+using Uno.Toolkit.UITest.Framework;
+using System.Drawing;
+
+namespace Uno.Toolkit.UITest.Controls.DrawerFlyout
+{
+	public class Given_DrawerFlyout : TestBase
+	{
+		protected override string SampleName => "DrawerFlyout";
+
+		[Test]
+		public void When_Left_DrawerFlyout()
+		{
+			CheckDrawerFlyout("Left");
+		}
+
+		[Test]
+		public void When_Top_DrawerFlyout()
+		{
+			CheckDrawerFlyout("Top");
+		}
+
+		[Test]
+		public void When_Right_DrawerFlyout()
+		{
+			CheckDrawerFlyout("Right");
+		}
+
+		[Test]
+		public void When_Bottom_DrawerFlyout()
+		{
+			CheckDrawerFlyout("Bottom");
+		}
+
+		void CheckDrawerFlyout(string direction)
+		{
+			// Navigate to the DrawerFlyout sample page
+			NavigateToSample("DrawerFlyoutSamplePage");
+
+			// RootPage area rectangle
+			var rootPage = App.WaitForElement("RootPage").Single().Rect;
+			// Button to test the DrawerFlyout
+			var drawerButton = App.Marked($"{direction}DrawerButton");
+			// Button inside the DrawerFlyout
+			var drawerFlyoutButton = App.Marked($"{direction}DrawerFlyoutButton");
+
+			// Wait on the DrawerFlyout test button
+			App.WaitForElement(drawerButton);
+
+			// Tap on the DrawerFlyout test button
+			App.FastTap(drawerButton);
+
+			// Wait on the Button inside the DrawerFlyout
+			App.WaitForElement(drawerFlyoutButton);
+
+			// Assert the Content of the Button
+			Assert.AreEqual($"Button {direction} Drawer", drawerFlyoutButton.GetDependencyPropertyValue("Content")?.ToString());
+
+			// Assert if the DrawerFlyout is correctly opened by checking the LightDismissOverlayBackground color
+			using var screenshotOpened = TakeScreenshot($"{direction} drawer opened");
+			ImageAssert.HasColorAt(screenshotOpened, rootPage.CenterX, rootPage.CenterY, Color.FromArgb(255, 191, 191, 191), 8);
+
+			// Tap in the middle of the LightDismissOverlayBackground in order to close the DrawerFlyout
+			App.TapCoordinates(rootPage.CenterX, rootPage.CenterY);
+			App.TapCoordinates(rootPage.CenterX, rootPage.CenterY);
+
+			// Assert if the DrawerFlyout is correctly closed by checking the LightDismissOverlayBackground color
+			using var screenshotClosed = TakeScreenshot($"{direction} drawer closed");
+			ImageAssert.HasColorAt(screenshotClosed, rootPage.CenterX, rootPage.CenterY, Color.White, 8);
+		}
+	}
+}
