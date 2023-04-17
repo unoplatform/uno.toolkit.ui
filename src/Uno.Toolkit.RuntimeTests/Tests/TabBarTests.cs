bb715a0c6487f65c8cf2a8ce5a113be5a3118a85
Steve Bilogan <steve.bilogan@gmail.com>
2023-04-17 14:08:59 -0400

    feat: Add IndicatorPlacement to TabBar (#540)
    * feat: Add IndicatorPlacement to TabBar

--- src/Uno.Toolkit.RuntimeTests/Tests/TabBarTests.cs
+++ src/Uno.Toolkit.RuntimeTests/Tests/TabBarTests.cs
@@ -10,17 +10,21 @@ using Uno.Toolkit.UI;
 using Uno.UI.RuntimeTests;
 using Uno.UI.Extensions;
 using Windows.Foundation;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI;
+using Windows.Foundation.Metadata;
 
 #if IS_WINUI
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml;
 using Microsoft.UI;
 using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
 #else
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml;
-using Windows.UI;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 #endif
 
 namespace Uno.Toolkit.RuntimeTests.Tests
@@ -259,6 +263,129 @@ namespace Uno.Toolkit.RuntimeTests.Tests
 					Orientation.Vertical =>  padding[0] + padding[2] + tabBarItem.ActualWidth,
 					_ => throw new ArgumentOutOfRangeException(nameof(orientation))
 				};
+			}
+		}
+
+		[TestMethod]
+		public async Task Verify_Indicator_Display_On_Selection()
+		{
+			if (!ImageAssertHelper.IsScreenshotSupported())
+			{
+				Assert.Inconclusive(); // System.NotImplementedException: RenderTargetBitmap is not supported on this platform.;
+			}
+
+			const int NumItems = 3;
+			var SUT = new TabBar
+			{
+				Background = new SolidColorBrush(Colors.Transparent),
+				SelectionIndicatorContentTemplate = XamlHelper.LoadXaml<DataTemplate>(@"
+					<DataTemplate>
+						<Border Background=""Red"" />
+					</DataTemplate>
+				"),
+				SelectionIndicatorPlacement = IndicatorPlacement.Above,
+			};
+
+			foreach (var item in Enumerable.Range(0, NumItems).Select(_ => CreateItem()))
+			{
+				SUT.Items.Add(item);
+			}
+
+			await UnitTestUIContentHelperEx.SetContentAndWait(SUT);
+
+		
+
+			for (int i = 0; i < NumItems; i++)
+			{
+				SUT.SelectedIndex = i;
+				await UnitTestsUIContentHelper.WaitForIdle();
+
+				var selectedItem = SUT.ContainerFromItem(SUT.SelectedItem) as TabBarItem;
+				Assert.IsNotNull(selectedItem);
+
+				var renderer = await SUT.TakeScreenshot();
+				var centerPoint = selectedItem!.TransformToVisual(SUT).TransformPoint(new Point(selectedItem.ActualWidth / 2, selectedItem.ActualHeight / 2));
+				
+				await renderer.AssertColorAt(Colors.Red, (int)centerPoint.X, (int)centerPoint.Y);
+
+				foreach (var nonSelected in SUT.Items.Cast<TabBarItem>().Where(x => !x.IsSelected))
+				{
+					centerPoint = nonSelected.TransformToVisual(SUT).TransformPoint(new Point(nonSelected.ActualWidth / 2, nonSelected.ActualHeight / 2));
+					await renderer.AssertColorAt(Colors.Green, (int)centerPoint.X, (int)centerPoint.Y);
+				}
+			}
+
+			TabBarItem CreateItem()
+			{
+				return new TabBarItem
+				{
+					Content = new Border
+					{
+						Padding = new Thickness(20),
+						Background = new SolidColorBrush(Colors.Green),
+					}
+				};
+			}
+		}
+
+		[TestMethod]
+		[DataRow(IndicatorPlacement.Above, DisplayName = "Verify Indicator Above")]
+		[DataRow(IndicatorPlacement.Below, DisplayName = "Verify Indicator Below")]
+		public async Task Verify_Indicator_Placement(IndicatorPlacement placement)
+		{
+			var item = new TabBarItem
+			{
+				Content = new Border
+				{
+					Padding = new Thickness(20),
+					Background = new SolidColorBrush(Colors.Green),
+				}
+			};
+
+			var SUT = new TabBar
+			{
+				Background = new SolidColorBrush(Colors.Transparent),
+				SelectionIndicatorContentTemplate = XamlHelper.LoadXaml<DataTemplate>(@"
+					<DataTemplate>
+						<Border Background=""Red"" />
+					</DataTemplate>
+				"),
+				SelectedIndex = 0,
+				SelectionIndicatorPlacement = placement,
+			};
+			SUT.Items.Add(item);
+
+			await UnitTestUIContentHelperEx.SetContentAndWait(SUT);
+
+			var belowPresenter = VisualTreeHelperEx
+				.GetFirstDescendant<TabBarSelectionIndicatorPresenter>(SUT, x => x.Name == "BelowSelectionIndicatorPresenter");
+			var abovePresenter = VisualTreeHelperEx
+				.GetFirstDescendant<TabBarSelectionIndicatorPresenter>(SUT, x => x.Name == "AboveSelectionIndicatorPresenter");
+
+			
+			var renderer = await SUT.TakeScreenshot();
+			var centerPoint = item.TransformToVisual(SUT).TransformPoint(new Point(item.ActualWidth / 2, item.ActualHeight / 2));
+			
+
+			if (placement == IndicatorPlacement.Above)
+			{
+				Assert.AreEqual(Visibility.Collapsed, belowPresenter!.Visibility);
+				Assert.AreEqual(Visibility.Visible, abovePresenter!.Visibility);
+
+				if (renderer is { })
+				{
+					await renderer.AssertColorAt(Colors.Red, (int)centerPoint.X, (int)centerPoint.Y);
+				}
+			}
+			else
+			{
+				Assert.AreEqual(Visibility.Visible, belowPresenter!.Visibility);
+				Assert.AreEqual(Visibility.Collapsed, abovePresenter!.Visibility);
+
+				if (renderer is { })
+				{
+					await renderer.AssertColorAt(Colors.Green, (int)centerPoint.X, (int)centerPoint.Y);
+				}
 			}
 		}
 	}
