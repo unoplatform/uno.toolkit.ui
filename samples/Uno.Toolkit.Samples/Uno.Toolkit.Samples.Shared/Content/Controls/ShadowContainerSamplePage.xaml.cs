a77bd570176b2d2c2778b7d834affe93718db166
roubachof <jm.alfonsi@gmail.com>
2023-06-06 10:11:42 +0100

    style: fixes after pr review

--- samples/Uno.Toolkit.Samples/Uno.Toolkit.Samples.Shared/Content/Controls/ShadowContainerSamplePage.xaml.cs
+++ samples/Uno.Toolkit.Samples/Uno.Toolkit.Samples.Shared/Content/Controls/ShadowContainerSamplePage.xaml.cs
@@ -9,13 +9,8 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 #endif
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace Uno.Toolkit.Samples.Content.Controls
 {
-	/// <summary>
-	/// An empty page that can be used on its own or navigated to within a Frame.
-	/// </summary>
 	[SamplePage(SampleCategory.Controls, nameof(ShadowContainer), Description = "Add many colored shadows to your controls.")]
 	public sealed partial class ShadowContainerSamplePage : Page
 	{
