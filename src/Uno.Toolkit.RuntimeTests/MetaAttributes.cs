6255f3fc70950bebc1b1d322f58b514daccc6ebb
Jerome Laban <jerome.laban@nventive.com>
2023-06-14 12:04:38 -0400

    feat!: Move to uno 5
    BREAKING CHANGE: Xamarin and .NET 6 targets are now removed

--- src/Uno.Toolkit.RuntimeTests/MetaAttributes.cs
+++ src/Uno.Toolkit.RuntimeTests/MetaAttributes.cs
@@ -1,6 +1,6 @@
 ﻿namespace System.Runtime.CompilerServices
 {
-#if !NET5 && !NET6_0_OR_GREATER
+#if !NET5 && !NET7_0_OR_GREATER
 	/// <summary>
 	/// Reserved to be used by the compiler for tracking metadata. This class should not be used by developers in source code.
 	/// </summary>
