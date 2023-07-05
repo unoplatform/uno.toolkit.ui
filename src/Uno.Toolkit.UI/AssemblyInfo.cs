8d8a4b5b92bde99fe3061ff97a5eefb1f6a17fc9
xiaoy312 <xiaoyao312@gmail.com>
2023-07-05 14:52:14 -0400

    chore: adjusting mobile heads
    (cherry picked from commit bd303196507f3623d40b7dd92339b020a7e24a8c)
    
    # Conflicts:
    #	samples/Directory.Packages.props
    #	src/Uno.Toolkit.sln

--- src/Uno.Toolkit.UI/AssemblyInfo.cs
+++ src/Uno.Toolkit.UI/AssemblyInfo.cs
@@ -5,17 +5,13 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("Uno.Toolkit.Samples")]
 #if IS_WINUI
 [assembly: InternalsVisibleTo("Uno.Toolkit.WinUI.Samples")]
-[assembly: InternalsVisibleTo("Uno.Toolkit.WinUI.Samples.Droid")]
-[assembly: InternalsVisibleTo("Uno.Toolkit.WinUI.Samples.iOS")]
 [assembly: InternalsVisibleTo("Uno.Toolkit.WinUI.Samples.Windows.Desktop")]
-[assembly: InternalsVisibleTo("Uno.Toolkit.WinUI.Samples.macOS")]
+[assembly: InternalsVisibleTo("Uno.Toolkit.WinUI.Samples.Mobile")]
 [assembly: InternalsVisibleTo("Uno.Toolkit.WinUI.Samples.WASM")]
 [assembly: InternalsVisibleTo("Uno.Toolkit.WinUI.Samples.Skia.Gtk")]
 #else
-[assembly: InternalsVisibleTo("Uno.Toolkit.Samples.Droid")]
-[assembly: InternalsVisibleTo("Uno.Toolkit.Samples.iOS")]
 [assembly: InternalsVisibleTo("Uno.Toolkit.Samples.UWP")]
-[assembly: InternalsVisibleTo("Uno.Toolkit.Samples.macOS")]
+[assembly: InternalsVisibleTo("Uno.Toolkit.Samples.Mobile")]
 [assembly: InternalsVisibleTo("Uno.Toolkit.Samples.WASM")]
 [assembly: InternalsVisibleTo("Uno.Toolkit.Samples.Skia.Gtk")]
 #endif
