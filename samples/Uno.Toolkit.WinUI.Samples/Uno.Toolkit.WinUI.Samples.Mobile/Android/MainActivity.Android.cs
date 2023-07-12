4b5ce43654fbabc3da8dfafd04f6ebb4c9a33bf0
xiaoy312 <xiaoyao312@gmail.com>
2023-07-12 11:13:41 -0400

    fix: mobile ui-tests not working

--- samples/Uno.Toolkit.WinUI.Samples/Uno.Toolkit.WinUI.Samples.Mobile/Android/MainActivity.Android.cs
+++ samples/Uno.Toolkit.WinUI.Samples/Uno.Toolkit.WinUI.Samples.Mobile/Android/MainActivity.Android.cs
@@ -3,14 +3,85 @@ using Android.Widget;
 using Android.OS;
 using Android.Content.PM;
 using Android.Views;
+using Java.Interop;
+using Uno.Toolkit.Samples;
+using System.IO;
+using System;
+using System.Threading;
 
 namespace Uno.Toolkit.WinUI.Samples.Droid;
 
 [Activity(
 	MainLauncher = true,
 	ConfigurationChanges = global::Uno.UI.ActivityHelper.AllConfigChanges,
+	// SoftInput.AdjustNothing is required by SafeArea
 	WindowSoftInputMode = SoftInput.AdjustNothing | SoftInput.StateHidden
 )]
 public class MainActivity : Microsoft.UI.Xaml.ApplicationActivity
 {
+#if USE_UITESTS
+	private HandlerThread _pixelCopyHandlerThread;
+
+	[Export("NavBackFromNestedPage")]
+	public void NavBackFromNestedPage() => App.NavBackFromNestedPage();
+
+	[Export("ForceNavigation")]
+	public void ForceNavigation(string sampleName) => App.ForceNavigation(sampleName);
+
+	[Export("ExitNestedSample")]
+	public void ExitNestedSampleBackdoor() => App.ExitNestedSample();
+
+	[Export("NavigateToNestedSample")]
+	public void NavigateToNestedSample(string pageName) => App.NavigateToNestedSample(pageName);
+
+	[Export("GetDisplayScreenScaling")]
+	public string GetDisplayScreenScaling(string value) => App.GetDisplayScreenScaling(value);
+
+	/// <summary>
+	/// Returns a base64 encoded PNG file
+	/// </summary>
+	[Export("GetScreenshot")]
+	public string GetScreenshot(string displayId)
+	{
+		// Get true size of screen, including status bar and bottom navigation bar
+		var metrics = Resources.DisplayMetrics;
+		var bitmap = Android.Graphics.Bitmap.CreateBitmap(metrics.WidthPixels, metrics.HeightPixels, Android.Graphics.Bitmap.Config.Argb8888);
+
+		if (_pixelCopyHandlerThread == null)
+		{
+			_pixelCopyHandlerThread = new Android.OS.HandlerThread("ScreenshotHelper");
+			_pixelCopyHandlerThread.Start();
+		}
+
+		var listener = new PixelCopyListener();
+
+		// PixelCopy.Request returns the actual rendering of the screen location for the app, including OpenGL content.
+		// Setting srcRect to null ensures that the entire screen, including status bar and bottom navigation bar, are captured.
+#pragma warning disable CA1416 // Validate platform compatibility
+		PixelCopy.Request(Window, srcRect: null, bitmap, listener, new Android.OS.Handler(_pixelCopyHandlerThread.Looper));
+#pragma warning restore CA1416 // Validate platform compatibility
+
+		listener.WaitOne();
+
+		using var memoryStream = new MemoryStream();
+		bitmap.Compress(Android.Graphics.Bitmap.CompressFormat.Png, 100, memoryStream);
+
+		return Convert.ToBase64String(memoryStream.ToArray());
+	}
+
+	class PixelCopyListener : Java.Lang.Object, PixelCopy.IOnPixelCopyFinishedListener
+	{
+		private ManualResetEvent _event = new ManualResetEvent(false);
+
+		public void WaitOne()
+		{
+			_event.WaitOne();
+		}
+
+		public void OnPixelCopyFinished(int copyResult)
+		{
+			_event.Set();
+		}
+	}
+#endif
 }
