1de0664573804340c4d18d0f256107bfb4347356
eriklimakc <erik.lima@knowcode.app>
2023-07-12 18:47:55 +0100

    fix: Fix iOS Xamarin.Calabash call on NET7

--- samples/Uno.Toolkit.Samples/Uno.Toolkit.Samples.Shared/App.xaml.cs
+++ samples/Uno.Toolkit.Samples/Uno.Toolkit.Samples.Shared/App.xaml.cs
@@ -84,7 +84,7 @@ namespace Uno.Toolkit.Samples
 		/// <param name="e">Details about the launch request and process.</param>
 		protected override async void OnLaunched(XamlLaunchActivatedEventArgs e)
 		{
-#if __IOS__ && !NET6_0 && USE_UITESTS
+#if __IOS__ && !NET6_0_OR_GREATER && USE_UITESTS
 			Xamarin.Calabash.Start();
 #endif
 
