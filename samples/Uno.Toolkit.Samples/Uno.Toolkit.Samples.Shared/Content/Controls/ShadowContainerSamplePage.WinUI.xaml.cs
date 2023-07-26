111b2a2adefd722f650915ecf58fcc039025178e
eriklimakc <erik.lima@knowcode.app>
2023-07-26 14:12:24 +0100

    chore: Cleaning code

--- samples/Uno.Toolkit.Samples/Uno.Toolkit.Samples.Shared/Content/Controls/ShadowContainerSamplePage.WinUI.xaml.cs
+++ samples/Uno.Toolkit.Samples/Uno.Toolkit.Samples.Shared/Content/Controls/ShadowContainerSamplePage.WinUI.xaml.cs
@@ -15,32 +15,6 @@ namespace Uno.Toolkit.Samples.Content.Controls
 		public ShadowContainerSamplePage()
 		{
 			this.InitializeComponent();
-
-			this.Loaded += (s, e) =>
-			{
-				var shadowContainer = SamplePageLayout.GetSampleChild<ShadowContainer>(Design.Agnostic, "ShadowContainer");
-				_shadows = shadowContainer.Shadows;
-
-				var shadowsItemsControl = SamplePageLayout.GetSampleChild<ItemsControl>(Design.Agnostic, "ShadowsItemsControl");
-				shadowsItemsControl.ItemsSource = _shadows;
-			};
-		}
-
-		private void AddShadow(object sender, RoutedEventArgs e)
-		{
-			var defaultShadow = (Shadow)Resources["DefaultShadow"];
-
-			_shadows.Add(defaultShadow.Clone());
-		}
-
-		private void RemoveShadow(object sender, RoutedEventArgs e)
-		{
-			if (_shadows.Count == 0)
-			{
-				return;
-			}
-
-			_shadows.RemoveAt(_shadows.Count - 1);
 		}
 	}
 }
