84b8a8068e0e717244484fe226e9498ad6eb2652
eriklimakc <erik.lima@knowcode.app>
2023-08-03 16:19:25 +0100

    chore: FAB styles and source to combobox

--- samples/Uno.Toolkit.Samples/Uno.Toolkit.Samples.Shared/Content/Controls/ShadowContainerSamplePage.WinUI.xaml.cs
+++ samples/Uno.Toolkit.Samples/Uno.Toolkit.Samples.Shared/Content/Controls/ShadowContainerSamplePage.WinUI.xaml.cs
@@ -4,17 +4,30 @@ using Uno.Toolkit.UI;
 
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using Uno.Toolkit.Samples.ViewModels;
 
 namespace Uno.Toolkit.Samples.Content.Controls
 {
-	[SamplePage(SampleCategory.Controls, nameof(ShadowContainer), Description = "Add many colored shadows to your controls.")]
+	[SamplePage(SampleCategory.Controls, nameof(ShadowContainer),
+		Description = "Add many colored shadows to your controls.",
+		DataType = typeof(ShadowContainerViewModel))]
 	public sealed partial class ShadowContainerSamplePage : Page
 	{
-		private ShadowCollection _shadows;
-
 		public ShadowContainerSamplePage()
 		{
 			this.InitializeComponent();
+		}
+
+		public class ShadowContainerViewModel : ViewModelBase
+		{
+			public ObservableCollection<string> CbbItems { get; } = new ObservableCollection<string>
+			{
+				"Item 1",
+				"Item 2",
+				"Item 3"
+			};
 		}
 	}
 }
