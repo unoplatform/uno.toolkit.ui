8b256c15be1236b3ed1d8d7c52b9fcbc0ba6095b
roubachof <jm.alfonsi@gmail.com>
2023-08-04 20:42:03 +0200

    feat(ShadowContainer): cache cleanup based on shadows stats

--- src/Uno.Toolkit.Skia.WinUI/Controls/Shadows/ShadowsCache.cs
+++ src/Uno.Toolkit.Skia.WinUI/Controls/Shadows/ShadowsCache.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
 using Microsoft.Extensions.Logging;
 using SkiaSharp;
 using Uno.Extensions;
@@ -8,9 +12,19 @@ using Uno.Logging;
 namespace Uno.Toolkit.UI;
 public class ShadowsCache
 {
+	private const int CleanupInterval = 30;
+
 	private static readonly ILogger _logger = typeof(ShadowsCache).Log();
 
+	private static readonly Predicate<CacheBucket> OneHitSinceAShortTime =
+		(bucket) => bucket.Hit == 1 && (DateTime.UtcNow - bucket.LastHit).TotalMinutes > 1;
+
+	private static readonly Predicate<CacheBucket> SeveralHitsSinceALongTime =
+		(bucket) => bucket.Hit > 1 && (DateTime.UtcNow - bucket.LastHit).TotalMinutes > 3;
+
 	private readonly ConcurrentDictionary<string, CacheBucket> _shadowsCache = new ConcurrentDictionary<string, CacheBucket>();
+
+	private DateTime _lastCleanupTime = DateTime.UtcNow;
 
 	public void AddOrUpdate(string key, SKImage image)
 	{
@@ -38,6 +52,8 @@ public class ShadowsCache
 				}
 				return existing;
 			});
+
+		CleanupIfNeeded();
 	}
 
 	public bool TryGetValue(string key, out SKImage? image)
@@ -100,5 +116,47 @@ public class ShadowsCache
 			Hit++;
 			LastHit = DateTime.UtcNow;
 		}
+	}
+
+	public async void CleanupIfNeeded()
+	{
+		if ((DateTime.UtcNow - _lastCleanupTime).TotalSeconds > CleanupInterval)
+		{
+			try
+			{
+				await CleanupAsync();
+			}
+			catch (Exception ex)
+			{
+				System.Diagnostics.Debug.WriteLine($"[ShadowsCache] Cleanup failed: {ex}");
+			}
+		}
+	}
+
+	/// <summary>
+	/// Removing old shadows based on their stats.
+	/// </summary>
+	private Task CleanupAsync()
+	{
+		_lastCleanupTime = DateTime.UtcNow;
+
+		return Task.Run(() =>
+		{
+			Stopwatch stopwatch = new Stopwatch();
+			stopwatch.Start();
+			System.Diagnostics.Debug.WriteLine($"[ShadowsCache] Cleanup starting");
+			int removedShadows = 0;
+			foreach (var expiredBucket in _shadowsCache
+				.Where(x => OneHitSinceAShortTime(x.Value) || SeveralHitsSinceALongTime(x.Value)))
+			{
+				if (_shadowsCache.TryRemove(expiredBucket.Key, out var _))
+				{
+					removedShadows++;
+				}
+			}
+
+			stopwatch.Stop();
+			System.Diagnostics.Debug.WriteLine($"[ShadowsCache] Cleanup done in {stopwatch.ElapsedMilliseconds} ms ({removedShadows} shadows removed)");
+		});		
 	}
 }
