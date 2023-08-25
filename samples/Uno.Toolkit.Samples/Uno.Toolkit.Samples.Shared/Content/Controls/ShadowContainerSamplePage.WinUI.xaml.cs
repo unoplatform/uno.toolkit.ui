03a42812ff3d2227470097f923fd02b8f15d1219
xiaoy312 <xiaoyao312@gmail.com>
2023-08-25 17:45:36 -0400

    chore: adding neumorphic samples for sanity check

--- samples/Uno.Toolkit.Samples/Uno.Toolkit.Samples.Shared/Content/Controls/ShadowContainerSamplePage.WinUI.xaml.cs
+++ samples/Uno.Toolkit.Samples/Uno.Toolkit.Samples.Shared/Content/Controls/ShadowContainerSamplePage.WinUI.xaml.cs
@@ -10,37 +10,9 @@ namespace Uno.Toolkit.Samples.Content.Controls
 	[SamplePage(SampleCategory.Controls, nameof(ShadowContainer), Description = "Add many colored shadows to your controls.")]
 	public sealed partial class ShadowContainerSamplePage : Page
 	{
-		private ShadowCollection _shadows;
-
 		public ShadowContainerSamplePage()
 		{
 			this.InitializeComponent();
-
-			this.Loaded += (s, e) =>
-			{
-				var shadowContainer = SamplePageLayout.GetSampleChild<ShadowContainer>(Design.Agnostic, "ShadowContainer");
-				_shadows = shadowContainer.Shadows;
-
-				var shadowsItemsControl = SamplePageLayout.GetSampleChild<ItemsControl>(Design.Agnostic, "ShadowsItemsControl");
-				shadowsItemsControl.ItemsSource = _shadows;
-			};
-		}
-
-		private void AddShadow(object sender, RoutedEventArgs e)
-		{
-			var defaultShadow = (Shadow)Resources["DefaultShadow"];
-
-			_shadows.Add(defaultShadow.Clone());
-		}
-
-		private void RemoveShadow(object sender, RoutedEventArgs e)
-		{
-			if (_shadows.Count == 0)
-			{
-				return;
-			}
-
-			_shadows.RemoveAt(_shadows.Count - 1);
 		}
 	}
 }
