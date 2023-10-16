ad6b4164e9d2ff073c7dc40732115e4b3b55ff2c
Silviu Obreja <silviu.obreja.dev@gmail.com>
2023-10-16 11:09:49 -0400

    chore: added Windows-specific conditional

--- src/Uno.Toolkit.UI/Behaviors/StatusBar.cs
+++ src/Uno.Toolkit.UI/Behaviors/StatusBar.cs
@@ -222,17 +222,21 @@ namespace Uno.Toolkit.UI
 		{
 			if (_lastActivePage is { } page)
 			{
+#if !HAS_UNO
 				page.GetDispatcherCompat().Schedule(() => {
+#endif
 					if (GetSubscription(page) != null &&
-					GetForeground(page) is StatusBarForegroundTheme.Auto or StatusBarForegroundTheme.AutoInverse &&
-					GetSystemTheme() is var theme && theme != _lastAppliedTheme)
-					{
-						// this will prevent deadlock, as setting the XamlStatusBar.Foreground will trigger ColorValuesChanged
-						_lastAppliedTheme = theme;
+						GetForeground(page) is StatusBarForegroundTheme.Auto or StatusBarForegroundTheme.AutoInverse &&
+						GetSystemTheme() is var theme && theme != _lastAppliedTheme)
+						{
+							// this will prevent deadlock, as setting the XamlStatusBar.Foreground will trigger ColorValuesChanged
+							_lastAppliedTheme = theme;
 
-						UpdateStatusBar(page);
-					}
+							UpdateStatusBar(page);
+						}
+#if !HAS_UNO
 				});
+#endif
 			}
 		}
 
