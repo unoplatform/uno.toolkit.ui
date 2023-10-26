163f1d8051fd4f0863d42f1b1f2ae95a11a87e94
Steve Bilogan <steve.bilogan@gmail.com>
2023-10-25 22:24:21 -0400

    build: add generators

---
+++ src/library/Uno.Toolkit.WinUI.Material.Markup/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using Uno.Extensions.Markup.Generator;
+
+[assembly: GenerateMarkupForAssembly(typeof(Uno.Toolkit.UI.Material.MaterialToolkitTheme))]
+[assembly: AssemblyMetadata("IsTrimmable", "True")]
