5583cb7d5ecc2bff626b2601f95681808ee4ccce
Martin Zikmund <martin@zikmund.dev>
2023-10-31 14:17:23 +0100

    fix: Shadow colors are no longer solid black on Android (#894)
    (cherry picked from commit 31028b21593d885d79873371c3d3271c72fa6482)

--- src/Uno.Toolkit.UI/Controls/CardContentControl/CardContentControl.cs
+++ src/Uno.Toolkit.UI/Controls/CardContentControl/CardContentControl.cs
@@ -34,12 +34,8 @@ namespace Uno.Toolkit.UI
 			public const string PointerFocused = nameof(PointerFocused);
 		}
 
-		private static readonly Windows.UI.Color DefaultShadowColor
-#if __ANDROID__
-			= Colors.Black;
-#else
-			= Windows.UI.Color.FromArgb(64, 0, 0, 0);
-#endif
+		private static readonly Windows.UI.Color DefaultShadowColor = 
+			Windows.UI.Color.FromArgb(64, 0, 0, 0);
 	}
 
 	/// <summary>
