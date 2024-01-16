57065bcf9f6e5327c5f34be662a4e3e76f4484cb
Nick Randolph <nick@builttoroam.com>
2024-01-17 02:17:38 +1100

    feat: Adding ProgressRing extension
    (cherry picked from commit b59f084ab2124ac9b448e0c030cd6b87c0459a59)

---
+++ src/Uno.Toolkit.UI/Behaviors/ProgressRingExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.System;
+using Microsoft.Extensions.Logging;
+using Uno.Extensions;
+using Uno.Logging;
+using System.Diagnostics.CodeAnalysis;
+using Uno.UI.Extensions;
+
+
+#if IS_WINUI
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+#else
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using ItemsRepeater = Microsoft.UI.Xaml.Controls.ItemsRepeater;
+#endif
+
+
+namespace Uno.Toolkit.UI
+{
+	public static class ProgressRingExtensions
+	{
+		public static readonly DependencyProperty IsActiveProperty =
+			DependencyProperty.RegisterAttached(
+				"IsActive",
+				typeof(bool),
+				typeof(ProgressRingExtensions),
+				new PropertyMetadata(false, IsActiveChanged));
+
+		private static void IsActiveChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			if (d is FrameworkElement element &&
+				e.NewValue is bool isActive)
+			{				
+				foreach (var item in element.EnumerateDescendants().OfType<ProgressRing>())
+				{
+					item.IsActive = isActive;
+				}
+			}
+		}
+
+		public static void SetIsActive(this FrameworkElement element, bool value)
+		{
+			element.SetValue(IsActiveProperty, value);
+		}
+
+		public static bool GetIsActive(this FrameworkElement element)
+		{
+			return (bool)element.GetValue(IsActiveProperty);
+		}
+	}
+}
