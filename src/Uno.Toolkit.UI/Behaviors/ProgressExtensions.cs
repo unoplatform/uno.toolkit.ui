1aee150147b414a2370406f59f9124099f96bf84
Nick Randolph <nick@builttoroam.com>
2024-01-20 03:48:45 +1100

    chore: Fixing windows build

--- src/Uno.Toolkit.UI/Behaviors/ProgressExtensions.cs
+++ src/Uno.Toolkit.UI/Behaviors/ProgressExtensions.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using Windows.System;
-using Microsoft.Extensions.Logging;
-using Uno.Extensions;
-using Uno.Logging;
-using System.Diagnostics.CodeAnalysis;
 using Uno.UI.Extensions;
-
 
 #if IS_WINUI
 using Microsoft.UI.Xaml;
@@ -20,9 +10,9 @@ using Microsoft.UI.Xaml.Input;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
 using ItemsRepeater = Microsoft.UI.Xaml.Controls.ItemsRepeater;
 #endif
-
 
 namespace Uno.Toolkit.UI
 {
@@ -39,14 +29,14 @@ namespace Uno.Toolkit.UI
 		{
 			if (d is FrameworkElement element &&
 				e.NewValue is bool isActive)
-			{				
+			{
 				foreach (var item in element.EnumerateDescendants())
 				{
-					if(item is ProgressRing progressRing)
+					if (item is ProgressRing progressRing)
 					{
 						progressRing.IsActive = isActive;
 					}
-					else if(item is ProgressBar progressBar)
+					else if (item is ProgressBar progressBar)
 					{
 						progressBar.IsIndeterminate = isActive;
 					}
@@ -63,5 +53,26 @@ namespace Uno.Toolkit.UI
 		{
 			return (bool)element.GetValue(IsActiveProperty);
 		}
+
+#if WINDOWS_UWP || WINDOWS
+		// Copied from Uno.UI.Extensions.ViewExtensions for Windows targets
+		private static IEnumerable<DependencyObject> EnumerateDescendants(this DependencyObject reference)
+		{
+			foreach (DependencyObject child in reference.EnumerateChildren())
+			{
+				yield return child;
+				foreach (DependencyObject item in child.EnumerateDescendants())
+				{
+					yield return item;
+				}
+			}
+		}
+
+		private static IEnumerable<DependencyObject> EnumerateChildren(this DependencyObject reference)
+		{
+			return from x in Enumerable.Range(0, VisualTreeHelper.GetChildrenCount(reference))
+											  select VisualTreeHelper.GetChild(reference, x);
+		}
+#endif
 	}
 }
