3608c1de8c35d3a650361f6c7991f1228322ad0b
Steve Bilogan <steve.bilogan@gmail.com>
2024-03-19 14:50:20 -0400

    fix: register events when navbarpresenter owner is set (#1076)
    * fix: register events when navbarpresenter owner is set
    
    * chore: whitespace
    
    * chore: pr comments
    
    Co-authored-by: Xiaotian Gu <xiaoyao312@gmail.com>
    
    ---------
    
    Co-authored-by: Xiaotian Gu <xiaoyao312@gmail.com>

--- src/Uno.Toolkit.UI/Controls/NavigationBar/NavigationBarPresenter.cs
+++ src/Uno.Toolkit.UI/Controls/NavigationBar/NavigationBarPresenter.cs
@@ -48,7 +48,9 @@ namespace Uno.Toolkit.UI
 			}
 
 			_weakNavBar = new WeakReference<NavigationBar?>(owner);
+
 			SetBindings();
+			RegisterEvents();
 		}
 		protected override void OnApplyTemplate()
 		{
