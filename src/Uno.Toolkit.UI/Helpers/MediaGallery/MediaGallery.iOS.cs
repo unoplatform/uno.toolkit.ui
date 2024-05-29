d2824e0e473b132ddcb989a51dbd733c7cb9cd16
Martin Zikmund <martin@zikmund.dev>
2024-05-29 15:33:27 +0200

    chore: iOS permissions adjustment

--- src/Uno.Toolkit.UI/Helpers/MediaGallery/MediaGallery.iOS.cs
+++ src/Uno.Toolkit.UI/Helpers/MediaGallery/MediaGallery.iOS.cs
@@ -1,67 +1,108 @@
 ﻿#if __IOS__
+using Photos;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using UIKit;
 
 namespace Uno.Toolkit.UI;
 
 public partial class MediaGallery
 {
-	private Task<bool> CheckAccessPlatformAsync()
+	private static readonly string _requiredInfoPlistKey = HasOSVersion(14) ? "NSPhotoLibraryAddUsageDescription" : "NSPhotoLibraryUsageDescription";
+
+	private static Task<bool> CheckAccessPlatformAsync()
 	{
-		throw new NotImplementedException();
+		if (NSBundle.MainBundle.InfoDictionary.ContainsKey(new NSString(usageKey)))
+		{
+
+		}
+		var auth = HasOSVersion(14) ?
+			PHPhotoLibrary.GetAuthorizationStatus(PHAccessLevel.AddOnly) :
+			PHPhotoLibrary.AuthorizationStatus;
+
+		return Task.FromResult(Convert(auth));
 	}
 
-	public Task SavePlatformAsync(MediaFileType type, Stream stream, string targetFileName)
+	/// <summary>Request <see cref="SaveMediaPermission"/> from the user.</summary>
+	/// <returns>The status of the permission that was requested.</returns>
+	public override async Task<PermissionStatus> RequestAsync()
 	{
-		throw new NotImplementedException();
-		//var tempFile = Path.Combine(Path.GetTempPath(), targetFileName);
-		//try
-		//{
-		//	// Write stream copy to temp
-		//	using var fileStream = File.Create(tempFile);
-		//	await stream.CopyToAsync(fileStream);
+		var status = await CheckStatusAsync();
+		if (status == PermissionStatus.Granted)
+			return status;
 
-		//	// get the file uri
-		//	var fileUri = new NSUrl(tempFile);
+		var auth = HasOSVersion(14)
+			? await PHPhotoLibrary.RequestAuthorizationAsync(PHAccessLevel.AddOnly)
+			: await PHPhotoLibrary.RequestAuthorizationAsync();
 
-		//	await PhotoLibraryPerformChanges(() =>
-		//	{
-		//		using var request = type == MediaFileType.Video
-		//		? PHAssetChangeRequest.FromVideo(fileUri)
-		//		: PHAssetChangeRequest.FromImage(fileUri);
-		//	}).ConfigureAwait(false);
-
-		//}
-		//finally
-		//{
-		//	// Attempt to delete the file
-		//	File.Delete(tempFile);
-		//}
+		return Convert(auth);
 	}
 
-	//static async Task PhotoLibraryPerformChanges(Action action)
-	//{
-	//	var tcs = new TaskCompletionSource<Exception>(TaskCreationOptions.RunContinuationsAsynchronously);
+	PermissionStatus Convert(PHAuthorizationStatus status)
+		=> status switch
+		{
+			PHAuthorizationStatus.Authorized => PermissionStatus.Granted,
+			PHAuthorizationStatus.Limited => PermissionStatus.Granted,
+			PHAuthorizationStatus.Denied => PermissionStatus.Denied,
+			PHAuthorizationStatus.Restricted => PermissionStatus.Restricted,
+			_ => PermissionStatus.Unknown,
+		};
 
-	//	PHPhotoLibrary.SharedPhotoLibrary.PerformChanges(
-	//		() =>
-	//		{
-	//			try
-	//			{
-	//				action.Invoke();
-	//			}
-	//			catch (Exception ex)
-	//			{
-	//				tcs.TrySetResult(ex);
-	//			}
-	//		},
-	//		(success, error) =>
-	//			tcs.TrySetResult(error != null ? new NSErrorException(error) : null));
 
-	//	var exception = await tcs.Task;
-	//	if (exception != null)
-	//		throw exception;
-	//}
+	public static Task<MediaGallerySaveResult> SavePlatformAsync(MediaFileType type, Stream sourceStream, string targetFileName, bool overwrite)
+	{
+		throw new NotImplementedException();
+		var tempFile = Path.Combine(Path.GetTempPath(), targetFileName);
+		try
+		{
+			// Write stream copy to temp
+			using var fileStream = File.Create(tempFile);
+			await stream.CopyToAsync(fileStream);
+
+			// get the file uri
+			var fileUri = new NSUrl(tempFile);
+
+			await PhotoLibraryPerformChanges(() =>
+			{
+				using var request = type == MediaFileType.Video
+				? PHAssetChangeRequest.FromVideo(fileUri)
+				: PHAssetChangeRequest.FromImage(fileUri);
+			}).ConfigureAwait(false);
+
+		}
+		finally
+		{
+			// Attempt to delete the file
+			File.Delete(tempFile);
+		}
+	}
+
+	static async Task PhotoLibraryPerformChanges(Action action)
+	{
+		var tcs = new TaskCompletionSource<Exception>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+		PHPhotoLibrary.SharedPhotoLibrary.PerformChanges(
+			() =>
+			{
+				try
+				{
+					action.Invoke();
+				}
+				catch (Exception ex)
+				{
+					tcs.TrySetResult(ex);
+				}
+			},
+			(success, error) =>
+				tcs.TrySetResult(error != null ? new NSErrorException(error) : null));
+
+		var exception = await tcs.Task;
+		if (exception != null)
+			throw exception;
+	}
+
+	private static bool HasOSVersion(int major) => UIDevice.CurrentDevice.CheckSystemVersion(major, 0);
 }
 #endif
