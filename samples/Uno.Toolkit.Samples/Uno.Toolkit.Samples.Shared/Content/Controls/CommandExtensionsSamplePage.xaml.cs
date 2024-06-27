1feea94dd13fd32d42427539f7f8eea0980aaecb
xiaoy312 <xiaoyao312@gmail.com>
2024-06-27 16:07:26 -0400

    fix(CommandExtensions): parameter resolution & add support for Selector

--- samples/Uno.Toolkit.Samples/Uno.Toolkit.Samples.Shared/Content/Controls/CommandExtensionsSamplePage.xaml.cs
+++ samples/Uno.Toolkit.Samples/Uno.Toolkit.Samples.Shared/Content/Controls/CommandExtensionsSamplePage.xaml.cs
@@ -25,20 +25,25 @@ namespace Uno.Toolkit.Samples.Content.Controls
 
 		public class CommandExtensionsSamplePageVM : ViewModelBase
 		{
+			public string[] Fruits { get; } = new[] { "Apple", "Banana", "Cactus" };
+
 			public string InputDebugText { get => GetProperty<string>(); set => SetProperty(value); }
-			public string SelectionDebugText { get => GetProperty<string>(); set => SetProperty(value); }
+			public string ListViewDebugText { get => GetProperty<string>(); set => SetProperty(value); }
+			public string SelectorDebugText { get => GetProperty<string>(); set => SetProperty(value); }
 			public string NavigationDebugText { get => GetProperty<string>(); set => SetProperty(value); }
 			public string ItemsRepeaterDebugText { get => GetProperty<string>(); set => SetProperty(value); }
 			public string ElementDebugText { get => GetProperty<string>(); set => SetProperty(value); }
 
 			public ICommand DebugInputCommand => new Command(DebugInput);
-			public ICommand DebugSelectionCommand => new Command(DebugSelection);
+			public ICommand DebugListViewCommand => new Command(DebugListView);
+			public ICommand DebugSelectorCommand => new Command(DebugSelector);
 			public ICommand DebugNavigationCommand => new Command(DebugNavigation);
 			public ICommand DebugItemsRepeaterCommand => new Command(DebugItemsRepeater);
 			public ICommand DebugElementTappedCommand => new Command(DebugElement);
 
 			private void DebugInput(object parameter) => InputDebugText = Invariant($"{DateTime.Now:HH:mm:ss}: parameter={parameter}");
-			private void DebugSelection(object parameter) => SelectionDebugText = Invariant($"{DateTime.Now:HH:mm:ss}: parameter={parameter}");
+			private void DebugListView(object parameter) => ListViewDebugText = Invariant($"{DateTime.Now:HH:mm:ss}: parameter={parameter}");
+			private void DebugSelector(object parameter) => SelectorDebugText = Invariant($"{DateTime.Now:HH:mm:ss}: parameter={parameter}");
 			private void DebugNavigation(object parameter) => NavigationDebugText = Invariant($"{DateTime.Now:HH:mm:ss}: parameter={parameter}");
 			private void DebugItemsRepeater(object parameter) => ItemsRepeaterDebugText = Invariant($"{DateTime.Now:HH:mm:ss}: parameter={parameter}");
 			private void DebugElement(object parameter) => ElementDebugText = Invariant($"{DateTime.Now:HH:mm:ss}: parameter={parameter}");
