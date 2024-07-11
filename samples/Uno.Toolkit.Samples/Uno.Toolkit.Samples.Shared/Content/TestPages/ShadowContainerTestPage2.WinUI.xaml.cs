f938b3f7f6bb90717c9bde6f34aaa717bbf99adb
Ramez Ragaa <ramezragaa@proton.me>
2024-07-11 15:18:44 +0300

    test: add a regression test for reloading ShadowContainers on wasm

---
+++ samples/Uno.Toolkit.Samples/Uno.Toolkit.Samples.Shared/Content/TestPages/ShadowContainerTestPage2.WinUI.xaml.cs
@@ -0,0 +1,45 @@
+﻿#if IS_WINUI
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Uno.Toolkit.Samples.Entities;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+
+namespace Uno.Toolkit.Samples.Content.TestPages
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+
+	[SamplePage(SampleCategory.Tests, "ShadowContainerTest2")]
+	public sealed partial class ShadowContainerTestPage2 : Page
+	{
+		public ShadowContainerTestPage2()
+		{
+			this.InitializeComponent();
+		}
+
+		private void OnLoadButtonClick(object sender, RoutedEventArgs e)
+		{
+			sp.Children.Add(shadowContainer);
+		}
+
+		private void OnUnloadButtonClick(object sender, RoutedEventArgs e)
+		{
+			sp.Children.Remove(shadowContainer);
+		}
+	}
+}
+#endif
