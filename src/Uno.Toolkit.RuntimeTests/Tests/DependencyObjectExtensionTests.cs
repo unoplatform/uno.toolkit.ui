ca15b3145a84dbcd76713489cd0e19cc9f47feea
xiaoy312 <xiaoyao312@gmail.com>
2024-07-18 12:04:20 -0400

    fix: FindDependencyProperty AmbiguousMatchException

--- src/Uno.Toolkit.RuntimeTests/Tests/DependencyObjectExtensionTests.cs
+++ src/Uno.Toolkit.RuntimeTests/Tests/DependencyObjectExtensionTests.cs
@@ -15,11 +15,13 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 #endif
 
+using DependencyObjectExtensions = Uno.Toolkit.UI.DependencyObjectExtensions;
+
 namespace Uno.Toolkit.RuntimeTests.Tests;
 
 [TestClass]
 [RunsOnUIThread]
-internal class DependencyObjectExtensionTests
+internal partial class DependencyObjectExtensionTests
 {
 	[TestMethod]
 	public void When_Type_FindDependencyProperty()
@@ -59,5 +61,41 @@ internal class DependencyObjectExtensionTests
 		var dp = new Grid().FindDependencyProperty<int>(nameof(Grid.RowProperty));
 
 		Assert.AreEqual(Grid.RowProperty, dp);
+	}
+
+	[TestMethod]
+	public void When_FindDependencyProperty_Ambiguous()
+	{
+		var dp = DependencyObjectExtensions.FindDependencyPropertyInfo(typeof(Ambiguity), nameof(Ambiguity.SomeValue))
+			?? throw new Exception("FindDependencyPropertyInfo returned null");
+
+		Assert.AreEqual(Ambiguity.SomeValueProperty, dp.Definition);
+		Assert.AreEqual(typeof(int), dp.PropertyType);
+	}
+}
+
+internal partial class DependencyObjectExtensionTests
+{
+	private partial class AmbiguityBase : DependencyObject
+	{
+		public object? SomeValue { get; set; }
+	}
+	private class Ambiguity : AmbiguityBase
+	{
+		#region DependencyProperty: SomeValue
+
+		public static DependencyProperty SomeValueProperty { get; } = DependencyProperty.Register(
+			nameof(SomeValue),
+			typeof(int),
+			typeof(Ambiguity),
+			new PropertyMetadata(default(int)));
+
+		public new int SomeValue
+		{
+			get => (int)GetValue(SomeValueProperty);
+			set => SetValue(SomeValueProperty, value);
+		}
+
+		#endregion
 	}
 }
