4374df6746256c6dd387b4ce7e74f905e69cf9b8
Steve Bilogan <steve.bilogan@gmail.com>
2024-09-12 22:37:29 -0400

    chore: fix flaky shadow test (#1241)
    (cherry picked from commit dc6ccefe6049a45fdf9f19cee32294255333e40a)
    
    # Conflicts:
    #	src/Uno.Toolkit.UITest/Controls/ShadowContainer/Given_ShadowContainer2.cs

--- src/Uno.Toolkit.UITest/Controls/ShadowContainer/Given_ShadowContainer2.cs
+++ src/Uno.Toolkit.UITest/Controls/ShadowContainer/Given_ShadowContainer2.cs
@@ -19,6 +19,7 @@ namespace Uno.Toolkit.UITest.Controls.ShadowContainer
 		[Test]
 		public void When_Unloaded_Then_Loaded()
 		{
+			App.WaitForElement("shadowContainer");
 			using var screenshot1 = TakeScreenshot("initial");
 
 			var testStatus = App.MarkedAnywhere("testStatus");
@@ -29,7 +30,11 @@ namespace Uno.Toolkit.UITest.Controls.ShadowContainer
 			using var screenshot2 = TakeScreenshot("reloaded");
 
 			var rect = GetRectangle("shadowContainer");
+<<<<<<< HEAD
 			ImageAssert.AreAlmostEqual(screenshot1, screenshot2, rect, permittedPixelError: 10);
+=======
+			ImageAssert.AreAlmostEqual(screenshot1, screenshot2, rect);
+>>>>>>> dc6ccef (chore: fix flaky shadow test (#1241))
 		}
 
 		private Rectangle GetRectangle(string marked)
