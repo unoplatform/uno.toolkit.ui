78f7f7b143558c7b3cddd2ad99d03f6e8f76639c
vatsashah45 <vatsashah45@gmail.com>
2024-09-19 13:28:25 -0400

    feat: ZoomContentControl Sample

---
+++ samples/Uno.Toolkit.Samples/Uno.Toolkit.Samples.Shared/Content/Controls/ZoomContentControlSamplePage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml;
+
+namespace Uno.Toolkit.Samples.Content.Controls
+{
+	public sealed partial class ZoomContentControlSamplePage : Page
+	{
+		public ZoomContentControlSamplePage()
+		{
+			this.InitializeComponent();
+		}
+
+		private void OnZoomInClick(object sender, RoutedEventArgs e)
+		{
+			if (ZoomContent.ZoomLevel < ZoomContent.MaxZoomLevel)
+			{
+				ZoomContent.ZoomLevel += 0.2;
+			}
+		}
+
+		private void OnZoomOutClick(object sender, RoutedEventArgs e)
+		{
+			if (ZoomContent.ZoomLevel > ZoomContent.MinZoomLevel)
+			{
+				ZoomContent.ZoomLevel -= 0.2;
+			}
+		}
+
+		private void OnResetClick(object sender, RoutedEventArgs e)
+		{
+			ZoomContent.ResetZoom();
+		}
+	}
+}
