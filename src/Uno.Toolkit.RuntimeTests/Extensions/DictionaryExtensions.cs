96bcb9037cb439d767be56588169537bf65a32a5
xiaoy312 <xiaoyao312@gmail.com>
2024-12-02 02:08:22 -0500

    chore: add support for partial xaml parsing

---
+++ src/Uno.Toolkit.RuntimeTests/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno.Toolkit.RuntimeTests.Extensions;
+
+internal static class DictionaryExtensions
+{
+	/// <summary>
+	/// Combine two dictionaries into a new one.
+	/// </summary>
+	/// <typeparam name="TKey"></typeparam>
+	/// <typeparam name="TValue"></typeparam>
+	/// <param name="dict"></param>
+	/// <param name="other"></param>
+	/// <param name="preferOther"></param>
+	/// <param name="comparer"></param>
+	/// <returns></returns>
+	public static IDictionary<TKey,TValue> Combine<TKey, TValue>(
+		this IReadOnlyDictionary<TKey,TValue> dict,
+		IReadOnlyDictionary<TKey,TValue>? other,
+		bool preferOther = true,
+		IEqualityComparer<TKey>? comparer = null
+	) where TKey : notnull
+	{
+		var result = new Dictionary<TKey, TValue>(dict, comparer);
+		if (other is { })
+		{
+			foreach (var kvp in other)
+			{
+				if (preferOther || !result.ContainsKey(kvp.Key))
+				{
+					result[kvp.Key] = kvp.Value;
+				}
+			}
+		}
+
+		return result;
+	}
+}
---
+++ src/Uno.Toolkit.RuntimeTests/Extensions/StackExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Uno.Toolkit.RuntimeTests.Extensions;
+
+internal static class StackExtensions
+{
+	public static IEnumerable<T> PopWhile<T>(this Stack<T> stack, Func<T, bool> predicate)
+	{
+		while (stack.TryPeek(out var item) && predicate(item))
+		{
+			yield return stack.Pop();
+		}
+	}
+}
