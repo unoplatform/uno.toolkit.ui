1815d9b129ebbe978baeeab9bd1ee3cbeb02e694
xiaoy312 <xiaoyao312@gmail.com>
2025-01-06 13:43:12 -0500

    chore: fix tabbar indicator tests

--- src/Uno.Toolkit.UI/Helpers/VisualTreeHelperEx.cs
+++ src/Uno.Toolkit.UI/Helpers/VisualTreeHelperEx.cs
@@ -135,6 +135,10 @@ namespace Uno.Toolkit.UI
 			.OfType<T>()
 			.FirstOrDefault();
 
+		public static T? GetFirstDescendant<T>(this DependencyObject reference, string name) where T : FrameworkElement => GetDescendants(reference)
+			.OfType<T>()
+			.FirstOrDefault(x => x.Name == name);
+
 		/// <summary>
 		/// Returns the first descendant of a specified type that satisfies the <paramref name="predicate"/>.
 		/// </summary>
@@ -146,9 +150,8 @@ namespace Uno.Toolkit.UI
 			.OfType<T>()
 			.FirstOrDefault(predicate);
 
-		public static T GetFirstDescendantOrThrow<T>(this DependencyObject reference, string name) where T : FrameworkElement => GetDescendants(reference)
-			.OfType<T>()
-			.FirstOrDefault(x => x.Name == name) ??
+		public static T GetFirstDescendantOrThrow<T>(this DependencyObject reference, string name) where T : FrameworkElement =>
+			GetFirstDescendant<T>(reference, name) ??
 			throw new Exception($"Unable to find element: {typeof(T).Name}#{name}");
 
 		/// <summary>
