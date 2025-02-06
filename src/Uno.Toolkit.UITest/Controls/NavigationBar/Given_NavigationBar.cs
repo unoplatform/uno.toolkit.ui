1782ce456442176951e7b83c96d6f8c7007fd16c
Steve Bilogan <steve.bilogan@gmail.com>
2025-02-05 19:29:31 -0500

    chore: test

--- src/Uno.Toolkit.UITest/Controls/NavigationBar/Given_NavigationBar.cs
+++ src/Uno.Toolkit.UITest/Controls/NavigationBar/Given_NavigationBar.cs
@@ -107,44 +107,5 @@ namespace Uno.Toolkit.UITest.Controls.NavigationBar
 
 			App.WaitForNoElement("M3Page2NavBar", "Timed out waiting for no Page 2 Nav Bar");
 		}
-
-		[Test]
-		[AutoRetry]
-		public void NavBar_AppBarButton_With_Icon_Click()
-		{
-			NavigateToNestedSample("FluentNavigationBarSampleNestedPage");
-
-			PlatformHelpers.On(
-				iOS: () => App.Tap("FluentPage1NavBarPrimaryCommand3"),
-				Android: () => App.Tap("FluentPage1NavBarPrimaryCommand3")
-			);
-
-			App.WaitForElement("FluentPage2NavBar", "Timed out waiting for Page 2 Nav Bar");
-		}
-
-		[Test]
-		[AutoRetry]
-		public void NavBar_Can_Close_Flyout_With_MainCommand()
-		{
-			NavigateToNestedSample("FluentNavigationBarSampleNestedPage");
-			App.Tap("OpenPage2FlyoutButton");
-			App.WaitForElement("FluentPage2NavBar", "Timed out waiting for Page 2 Nav Bar");
-			App.Tap("NavigateToThirdButton");
-			App.WaitForElement("FluentPage3NavBar", "Timed out waiting for Page 3 Nav Bar");
-
-			PlatformHelpers.On(
-				iOS: () => App.Tap("FluentPage3NavBarMainCommand"),
-				Android: () => App.Tap(q => q.Marked("FluentPage3NavBar").Descendant("AppCompatImageButton"))
-			);
-
-			App.WaitForElement("FluentPage2NavBar", "Timed out waiting for Page 2 Nav Bar");
-
-			PlatformHelpers.On(
-				iOS: () => App.Tap("FluentPage2NavBarMainCommand"),
-				Android: () => App.Tap(q => q.Marked("FluentPage2NavBar").Descendant("AppCompatImageButton"))
-			);
-
-			App.WaitForElement("FluentPage1NavBar", "Timed out waiting for Page 1 Nav Bar");
-		}
 	}
 }
