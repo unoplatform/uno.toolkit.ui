9c3a9640b792378975d69b55280d438d044d0c42
Steve Bilogan <steve.bilogan@gmail.com>
2025-09-16 10:11:39 -0400

    ci: adjust android test builds

---
+++ build/workflow/scripts/android-uitest-build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+export UNO_UITEST_ANDROID_PROJECT_PATH=$BUILD_SOURCESDIRECTORY/samples/Uno.Toolkit.Samples/Uno.Toolkit.Samples
+
+cd "$UNO_UITEST_ANDROID_PROJECT_PATH"
+dotnet publish -f net9.0-android -c Release /p:AndroidPackageFormat=apk /p:RuntimeIdentifier=android-x64 /p:IsUiAutomationMappingEnabled=true /p:AndroidUseSharedRuntime=false /p:AndroidUseAssemblyStore=false /p:AndroidFastDeploymentType=None /p:AotAssemblies=false /p:RunAOTCompilation=false /p:PublishTrimmed=false /p:AndroidStripILAfterAOT=false -bl:"$BUILD_ARTIFACTSTAGINGDIRECTORY/android-uitest-build.binlog"
