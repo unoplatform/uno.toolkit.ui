3e8611999664fcb84e146b738a8287ee3b6e18ee
Kunal22shah <kunalshah606@gmail.com>
2025-10-02 14:41:54 -0400

    chore: resolve PR comments

--- src/Uno.Toolkit.UI/Controls/LoadingView/LoadingView.HotReload.cs
+++ src/Uno.Toolkit.UI/Controls/LoadingView/LoadingView.HotReload.cs
@@ -10,38 +10,32 @@ using Windows.UI.Xaml;
 using Uno.Toolkit.UI;
 
 [assembly: ElementMetadataUpdateHandlerAttribute(typeof(LoadingView), typeof(LoadingViewElementMetadataUpdateHandler))]
-[assembly: ElementMetadataUpdateHandlerAttribute(typeof(ExtendedSplashScreen), typeof(LoadingViewElementMetadataUpdateHandler))]
 
 namespace Uno.Toolkit.UI;
+
 internal static class LoadingViewElementMetadataUpdateHandler
 {
+	private const string SourceKey = nameof(LoadingView) + "." + nameof(LoadingView.Source);
+	private const string ContentKey = nameof(LoadingView) + "." + nameof(LoadingView.Content);
 
 	public static void CaptureState(FrameworkElement element, IDictionary<string, object> stateDictionary, Type[]? updatedTypes)
 	{
 		if (element is LoadingView lv)
 		{
-			if (lv.Source is { } src)
-			{
-				stateDictionary["LV.Source"] = src;
-			}
-			if (lv.Content is { } content)
-			{
-				stateDictionary["LV.Content"] = content;
-			}
+			stateDictionary[SourceKey] = lv.Source;
+			stateDictionary[ContentKey] = lv.Content;
 		}
-
 	}
 
 	public static Task RestoreState(FrameworkElement element, IDictionary<string, object> state, Type[]? updatedTypes)
 	{
 		if (element is LoadingView lv)
 		{
-			if (lv.Source is null && state.TryGetValue("LV.Source", out var savedSrc) && savedSrc is ILoadable src)
+			if (lv.Source is null && lv.GetBindingExpression(LoadingView.SourceProperty) is null && state.TryGetValue(SourceKey, out var savedSrc) && savedSrc is ILoadable src)
 			{
 				lv.Source = src;
 			}
-
-			if (lv.Content is null && state.TryGetValue("LV.Content", out var savedContent) && savedContent is { } content)
+			if (lv.Content is null && lv.GetBindingExpression(LoadingView.ContentProperty) is null && state.TryGetValue(ContentKey, out var savedContent) && savedContent is { } content)
 			{
 				lv.Content = content;
 			}
