0c4bdb57884650bab21e1947125377b9d3649a22
xiaoy312 <xiaoyao312@gmail.com>
2026-01-28 17:18:58 -0500

    fix(navbar): content alignment

--- src/Uno.Toolkit.UI/Extensions/FrameworkElementExtensions.cs
+++ src/Uno.Toolkit.UI/Extensions/FrameworkElementExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Uno.Disposables;
 
 #if IS_WINUI
 using Microsoft.UI.Xaml;
@@ -12,18 +13,88 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Data;
 #endif
 
-namespace Uno.Toolkit.UI
+namespace Uno.Toolkit.UI;
+
+internal static class FrameworkElementExtensions
 {
-	internal static class FrameworkElementExtensions
+	public static void InheritDataContextFrom(this FrameworkElement fe, FrameworkElement parent)
 	{
-		public static void InheritDataContextFrom(this FrameworkElement fe, FrameworkElement parent)
+		fe.SetBinding(FrameworkElement.DataContextProperty, new Binding
 		{
-			fe.SetBinding(FrameworkElement.DataContextProperty, new Binding
+			Source = parent,
+			Path = new PropertyPath(nameof(parent.DataContext)),
+			Mode = BindingMode.OneWay,
+		});
+	}
+
+	/// <summary>
+	/// Subscribes to the Loaded event of nested elements, as defined by the innerSelectors, with a callback on the innermost element loaded.
+	/// </summary>
+	/// <remarks>Used to wait through multiple layers of template materialization.</remarks>
+	/// <param name="root"></param>
+	/// <param name="innerSelectors"></param>
+	/// <param name="onInnerMostLoaded"></param>
+	/// <returns></returns>
+	public static IDisposable SubscribeToNestedElements(this FrameworkElement root, Func<FrameworkElement, FrameworkElement?>[] innerSelectors, Action<FrameworkElement> onInnerMostLoaded)
+	{
+		var subscriptions = new List<(int Depth, FrameworkElement Element, Action Unsubscribe)>();
+		var subscriptionsLock = new object();
+		var disposed = false;
+
+		Subscribe(root, 0);
+		return Disposable.Create(() =>
+		{
+			disposed = true;
+			subscriptions.Clear();
+		});
+
+		void Subscribe(FrameworkElement e, int depth)
+		{
+			if (disposed) return;
+
+			if (subscriptions.Count > depth)
 			{
-				Source = parent,
-				Path = new PropertyPath(nameof(parent.DataContext)),
-				Mode = BindingMode.OneWay,
-			});
+				if ((subscriptions.ElementAt(depth).Element != e))
+				{
+					// elment register at this depth is no longer the same
+					// drop everything from this depth and lower
+					lock (subscriptionsLock) subscriptions.RemoveRange(depth, subscriptions.Count - depth);
+
+					// and, push a new stack
+					RoutedEventHandler handler = (s, _) => Walk(e, depth);
+					e.Loaded += handler;
+					lock (subscriptionsLock) subscriptions.Add((depth, e, () => e.Loaded -= handler));
+				}
+				else
+				{
+					// same element reloaded, nothing to do
+				}
+			}
+			else
+			{
+				// new depth reached, just push a new stack
+				RoutedEventHandler handler = (s, _) => Walk(e, depth);
+				e.Loaded += handler;
+				lock (subscriptionsLock) subscriptions.Add((depth, e, () => e.Loaded -= handler));
+			}
+
+			if (e.IsLoaded)
+			{
+				Walk(e, depth);
+			}
+		}
+		void Walk(FrameworkElement e, int depth)
+		{
+			if (disposed) return;
+
+			if (depth >= innerSelectors.Length)
+			{
+				onInnerMostLoaded(e);
+			}
+			else if (innerSelectors[depth](e) is { } nested)
+			{
+				Subscribe(nested, depth + 1);
+			}
 		}
 	}
 }
