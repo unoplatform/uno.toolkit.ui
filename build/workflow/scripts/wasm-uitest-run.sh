f6ccba1d3dcacf4c6ddb4cec49c1d8f4c6b3e154
Steve Bilogan <steve.bilogan@gmail.com>
2026-02-26 14:12:54 -0500

    chore: tfm fixes

--- build/workflow/scripts/wasm-uitest-run.sh
+++ build/workflow/scripts/wasm-uitest-run.sh
@@ -33,7 +33,7 @@ TEST_FAILED_FLAG=.tests-failed
 
 cd $UNO_UITEST_WASM_PROJECT
 
-dotnet publish -f net10.0-browserwasm /p:SamplesTargetFrameworkOverride=net10.0-browserwasm /p:TargetFrameworkOverride=net10.0 /p:Configuration=Release /p:IsUiAutomationMappingEnabled=True /p:DisableMobileTargets=True /bl:$BASE_ARTIFACTS_PATH/wasm-uitest.binlog
+dotnet publish -f net10.0-browserwasm /p:TargetFrameworkOverride=browserwasm /p:Configuration=Release /p:IsUiAutomationMappingEnabled=True /p:DisableMobileTargets=True /bl:$BASE_ARTIFACTS_PATH/wasm-uitest.binlog
 cd $BUILD_SOURCESDIRECTORY/build
 mkdir -p tools
 
