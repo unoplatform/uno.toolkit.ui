78c97e90b35935e28b6179efb2eabd03c4d46f7d
Steve Bilogan <steve.bilogan@gmail.com>
2026-02-26 15:27:53 -0500

    chore: remove modal

--- samples/Uno.Toolkit.Samples/Content/Controls/SafeAreaSamplePage.xaml.cs
+++ samples/Uno.Toolkit.Samples/Content/Controls/SafeAreaSamplePage.xaml.cs
@@ -31,19 +31,5 @@ namespace Uno.Toolkit.Samples.Content.Controls
 		{
 			Shell.GetForCurrentView().ShowNestedSample<SafeArea_SoftInput_Scroll>(clearStack: true);
 		}
-
-		private void LaunchModalSample(object sender, RoutedEventArgs e)
-		{
-#if __IOS__
-			var vc = new UIViewController { View = new SafeArea_ModalPage() };
-			if (UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Pad)
-			{
-				// Esnure the behavior of the iPad modal presentation mimics that of the iPhone
-				vc.PreferredContentSize = Microsoft.UI.Xaml.Window.Current.Bounds.ToCGRect().Size;
-				vc.ModalPresentationStyle = UIModalPresentationStyle.FormSheet;
-			}
-			UIApplication.SharedApplication.KeyWindow.RootViewController.PresentModalViewController(vc, true);
-#endif
-		}
 	}
 }
