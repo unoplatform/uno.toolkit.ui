60f9156f8380aef696a5d0b7f1c0c2840975c2c5
Kunal22shah <kunalshah606@gmail.com>
2026-04-20 11:48:30 -0400

    feat: add Simple theme support to seed color sample page

--- samples/Uno.Toolkit.Samples/Content/Controls/SeedColorSamplePage.xaml.cs
+++ samples/Uno.Toolkit.Samples/Content/Controls/SeedColorSamplePage.xaml.cs
@@ -7,9 +7,9 @@ namespace Uno.Toolkit.Samples.Content.Controls;
 [SamplePage(
 	SampleCategory.Controls,
 	"Seed Color",
-	SourceSdk.UnoMaterial,
+	SourceSdk.UnoToolkit,
 	Description = "Generate a full color palette from a single seed color using the HCT color space.",
-	SupportedDesigns = new[] { Design.Material })]
+	SupportedDesigns = new[] { Design.Material, Design.Agnostic })]
 public sealed partial class SeedColorSamplePage : Page
 {
 	private static Color _lastSeed = Color.FromArgb(0xFF, 0x59, 0x46, 0xD2);
@@ -40,6 +40,15 @@ public sealed partial class SeedColorSamplePage : Page
 		SeedSwatch.Background = new SolidColorBrush(seed);
 		SeedHex.Text = $"#{seed.R:X2}{seed.G:X2}{seed.B:X2}";
 
+#if THEME_SIMPLE
+		XamlSnippet.Text =
+			$"<SimpleToolkitTheme xmlns=\"using:Uno.Toolkit.UI.Simple\">\n" +
+			$"  <SimpleToolkitTheme.Colors>\n" +
+			$"    <ThemeColors xmlns=\"using:Uno.Themes\"\n" +
+			$"                 PrimarySeed=\"#{seed.R:X2}{seed.G:X2}{seed.B:X2}\" />\n" +
+			$"  </SimpleToolkitTheme.Colors>\n" +
+			$"</SimpleToolkitTheme>";
+#else
 		XamlSnippet.Text =
 			$"<MaterialToolkitTheme xmlns=\"using:Uno.Toolkit.UI.Material\">\n" +
 			$"  <MaterialToolkitTheme.Colors>\n" +
@@ -47,5 +56,6 @@ public sealed partial class SeedColorSamplePage : Page
 			$"                 PrimarySeed=\"#{seed.R:X2}{seed.G:X2}{seed.B:X2}\" />\n" +
 			$"  </MaterialToolkitTheme.Colors>\n" +
 			$"</MaterialToolkitTheme>";
+#endif
 	}
 }
